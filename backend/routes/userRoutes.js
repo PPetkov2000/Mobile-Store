2da13764503957355c17177794146e7885052576
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-11-12 22:24:15 +0200

    add user routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const {
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  getUserProfile,
+  updateUserProfile,
+  addToFavourites,
+  registerUser,
+  authUser,
+} = require("../controllers/userController");
+const { isAuth, isAdmin } = require("../middleware/authMiddleware");
+
+router.route("/").get(isAuth, isAdmin, getUsers);
+router
+  .route("/profile")
+  .get(isAuth, getUserProfile)
+  .put(isAuth, updateUserProfile);
+router
+  .route("/:id")
+  .get(isAuth, isAdmin, getUserById)
+  .put(isAuth, isAdmin, updateUser)
+  .delete(isAuth, isAdmin, deleteUser);
+router.route("/:id/like").put(isAuth, addToFavourites);
+
+router.route("/register").post(registerUser);
+router.route("/login").post(authUser);
+
+module.exports = router;
