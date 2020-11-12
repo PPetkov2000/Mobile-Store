7d35e4298f6832f3552d50d85bca19c03f394002
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-11-12 22:20:34 +0200

    add server.js

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const connectDB = require("./config/database");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
