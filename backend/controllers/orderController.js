f7945a92bf3b102734713d3a06f9fec7fff1df5d
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-11-14 23:12:01 +0200

    add order controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,100 @@
+const Order = require("../models/OrderModel");
+
+const getOrders = async (req, res, next) => {
+  try {
+    const orders = await Order.find()
+      .populate("creator")
+      .sort({
+        createdAt: "desc",
+      })
+      .limit(10);
+    res.json(orders);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getMyOrders = async (req, res, next) => {
+  try {
+    const orders = await Order.find({ creator: req.user._id })
+      .sort({
+        createdAt: "desc",
+      })
+      .limit(10);
+    res.json(orders);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getOrderById = async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id).populate("creator");
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404);
+      throw new Error("Order not Found");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createOrder = async (req, res, next) => {
+  try {
+    if (req.body.orderItems && req.body.orderItems.length === 0) {
+      res.status(400);
+      throw new Error("No order items");
+    }
+
+    const order = new Order({
+      orderItems: req.body.orderItems,
+      shippingAddress: req.body.shippingAddress,
+      paymentMethod: req.body.paymentMethod,
+      itemsPrice: req.body.itemsPrice,
+      shippingPrice: req.body.shippingPrice,
+      taxPrice: req.body.taxPrice,
+      totalPrice: req.body.totalPrice,
+      creator: req.user._id,
+    });
+
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const payOrder = async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.payer.email_address,
+      };
+
+      const updatedOrder = await order.save();
+      res.json(updatedOrder);
+    } else {
+      res.status(404);
+      throw new Error("Order not Found");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getOrders,
+  getMyOrders,
+  getOrderById,
+  createOrder,
+  payOrder,
+};
