0ef6aff02fe598c746931d8d298558b2ac089aed
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-11-18 22:48:31 +0200

    add main files and test data

---
+++ frontend/src/App.js
@@ -0,0 +1,81 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Container } from "react-bootstrap";
+import RegisterScreen from "./screens/RegisterScreen";
+import LoginScreen from "./screens/LoginScreen";
+import CartScreen from "./screens/CartScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import UserListScreen from "./screens/admin/UserListScreen";
+import UserEditScreen from "./screens/admin/UserEditScreen";
+import ProductListScreen from "./screens/admin/ProductListScreen";
+import ProductEditScreen from "./screens/admin/ProductEditScreen";
+import OrderListScreen from "./screens/admin/OrderListScreen";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className="py-5">
+        <Route path="/" exact component={HomeScreen} />
+        <Route path="/search/:keyword" exact component={HomeScreen} />
+        <Route path="/page/:pageNumber" exact component={HomeScreen} />
+        <Route
+          path="/search/:keyword/page/:pageNumber"
+          exact
+          component={HomeScreen}
+        />
+        <Container>
+          <Route path="/products/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/order/:id" component={OrderScreen} />
+          <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/payment" component={PaymentScreen} />
+          <Route path="/placeorder" component={PlaceOrderScreen} />
+          <Route path="/profile" exact component={ProfileScreen} />
+          <Route
+            path="/profile/page/:pageNumber"
+            exact
+            component={ProfileScreen}
+          />
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/admin/userlist" exact component={UserListScreen} />
+          <Route
+            path="/admin/userlist/:pageNumber"
+            exact
+            component={UserListScreen}
+          />
+          <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+          <Route
+            path="/admin/productlist"
+            exact
+            component={ProductListScreen}
+          />
+          <Route
+            path="/admin/productlist/:pageNumber"
+            exact
+            component={ProductListScreen}
+          />
+          <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
+          <Route path="/admin/orderlist" exact component={OrderListScreen} />
+          <Route
+            path="/admin/orderlist/:pageNumber"
+            exact
+            component={OrderListScreen}
+          />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
