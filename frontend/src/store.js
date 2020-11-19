eb6983b2e43b4052addf6eb903a741a1a91ef358
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-11-19 04:34:16 +0200

    initialize redux store

---
+++ frontend/src/store.js
@@ -0,0 +1,82 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productCreateReducer,
+  productDeleteReducer,
+  productDetailsReducer,
+  productListReducer,
+  productReviewCreateReducer,
+  productReviewDeleteReducer,
+  productTopRatedReducer,
+  productUpdateReducer,
+} from "./reducers/productReducers";
+import {
+  userDetailsReducer,
+  userListReducer,
+  userLoginReducer,
+  userRegisterReducer,
+  userUpdateProfileReducer,
+  userUpdateReducer,
+  userDeleteReducer,
+  userAddFavouritesReducer,
+  userRemoveFavouritesReducer,
+} from "./reducers/userReducers";
+import { cartReducer } from "./reducers/cartReducers";
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderListMyReducer,
+  orderListReducer,
+  orderPayReducer,
+} from "./reducers/orderReducers";
+
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
+  productDelete: productDeleteReducer,
+  productReviewCreate: productReviewCreateReducer,
+  productReviewDelete: productReviewDeleteReducer,
+  productTopRated: productTopRatedReducer,
+  cart: cartReducer,
+  orderCreate: orderCreateReducer,
+  orderList: orderListReducer,
+  orderListMy: orderListMyReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
+  userRegister: userRegisterReducer,
+  userLogin: userLoginReducer,
+  userList: userListReducer,
+  userDetails: userDetailsReducer,
+  userUpdate: userUpdateReducer,
+  userDelete: userDeleteReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+  userAddFavourites: userAddFavouritesReducer,
+  userRemoveFavourites: userRemoveFavouritesReducer,
+});
+
+const initialState = {
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+    shippingAddress: localStorage.getItem("shippingAddress")
+      ? JSON.parse(localStorage.getItem("shippingAddress"))
+      : {},
+  },
+  userLogin: {
+    userInfo: localStorage.getItem("userInfo")
+      ? JSON.parse(localStorage.getItem("userInfo"))
+      : null,
+  },
+};
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
