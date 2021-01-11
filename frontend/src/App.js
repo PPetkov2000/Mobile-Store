1325f284ece45bc085a755535996006eef28a90b
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-01-11 23:45:25 +0200

    add sass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
