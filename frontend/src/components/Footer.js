41bccc9f02f7552b6ecc4f79dcf71d71cfe8cc87
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-01-12 23:36:19 +0200

    style footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer>
-      <h4>Footer</h4>
+    <footer className="py-5 bg-dark">
+      <div className="container">
+        <p className="m-0 text-center text-white">
+          Copyright © ng-mobile-store 2020
+        </p>
+      </div>
     </footer>
   );
 }
