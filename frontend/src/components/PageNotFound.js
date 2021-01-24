4baeff5a8dadf779a91aa4424c0687eb6988d620
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-01-24 18:41:44 +0200

    add 404 page

---
+++ frontend/src/components/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PageNotFound() {
+  return (
+    <div className="d-flex flex-column align-items-center mt-5">
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+    </div>
+  );
+}
+
+export default PageNotFound;
