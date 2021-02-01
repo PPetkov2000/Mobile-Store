bd22f20ac5c8567a3e4cb16797bdf6f4f3a539f9
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-02-02 00:22:51 +0200

    create products component

---
+++ frontend/src/components/Products.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Paginate from "./Paginate";
+import Product from "./Product";
+
+function Products({ title, icon, products, page, pages, keyword }) {
+  return (
+    <section className="products-container">
+      <h2 className="products-title">
+        {icon} {title}
+      </h2>
+      <div className="products-content">
+        <Row xs={1} sm={2} md={3} lg={4} xl={4}>
+          {products.map((product) => {
+            return (
+              <Col key={product._id}>
+                <Product product={product} />
+              </Col>
+            );
+          })}
+        </Row>
+        {page && <Paginate page={page} pages={pages} keyword={keyword} />}
+      </div>
+    </section>
+  );
+}
+
+export default Products;
