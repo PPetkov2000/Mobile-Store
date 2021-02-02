b084aeeafc8eed51937fc794fb69daa5713ac7aa
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-02-03 01:58:46 +0200

    create search form component

---
+++ frontend/src/components/ProductSearchForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Form, FormControl } from "react-bootstrap";
+import { listProducts } from "../actions/productActions";
+
+function ProductSearchForm() {
+  const [keyword, setKeyword] = useState("");
+  const dispatch = useDispatch();
+
+  const searchHandler = (e) => {
+    e.preventDefault();
+    dispatch(listProducts(keyword));
+    setKeyword("");
+  };
+
+  return (
+    <Form inline className="mx-auto" onSubmit={searchHandler}>
+      <FormControl
+        type="text"
+        placeholder="Search..."
+        className="mr-sm-2 searchInput"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+      <button type="submit" className="btn search-btn">
+        Search
+      </button>
+    </Form>
+  );
+}
+
+export default ProductSearchForm;
