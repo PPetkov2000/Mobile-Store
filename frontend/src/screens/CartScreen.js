ae9d1284b27fd0de4a8df40b675ea1acfdf5ff5a
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-02-04 14:20:34 +0200

    wrap the content inside a container

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -28,83 +28,88 @@ function CartScreen({ match, location, history }) {
   };
 
   return (
-    <Row>
-      <Col md={8} className="cart-info-column">
-        <h2 className="mb-3">Shopping Cart</h2>
-        {cartItems.length === 0 ? (
-          <Message>
-            Your cart is empty <Link to="/">Go Back</Link>
-          </Message>
-        ) : (
-          cartItems.map((item) => {
-            return (
-              <Row key={item.product} className="cart-info-item">
-                <Col md={1} sm={1} xs={1}>
-                  <img
-                    src={item.images && item.images[0]}
-                    alt={item.name}
-                    className="cart-image"
-                  />
-                </Col>
-                <Col md={5} sm={5} xs={5}>
-                  <Link
-                    to={`/products/${item.product}`}
-                    className="cart-product-name"
-                  >
-                    {item.name}
-                  </Link>
-                </Col>
-                <Col md={3} sm={3} xs={3}>
-                  ${item.price}
-                </Col>
-                <Col md={2} sm={2} xs={2}>
-                  <FormControl
-                    as="select"
-                    value={item.quantity}
-                    onChange={(e) =>
-                      dispatch(addToCart(item.product, Number(e.target.value)))
-                    }
-                  >
-                    {[...Array(item.countInStock).keys()].map((x) => (
-                      <option key={x + 1} value={x + 1}>
-                        {x + 1}
-                      </option>
-                    ))}
-                  </FormControl>
-                </Col>
-                <Col md={1} sm={1} xs={1}>
-                  <button
-                    type="button"
-                    className="cart-remove-item-btn"
-                    onClick={() => removeFromCartHandler(item.product)}
-                  >
-                    <i className="fa fa-trash"></i>
-                  </button>
-                </Col>
-              </Row>
-            );
-          })
-        )}
-      </Col>
-      <Col md={4}>
-        <Card className="cart-card">
-          <h5 className="cart-title">
-            Subtotal ({cartItems.reduce((acc, curr) => acc + curr.quantity, 0)}{" "}
-            {cartItems.length === 1 ? "item" : "items"}) : $
-            {cartItems
-              .reduce((acc, curr) => acc + curr.price * curr.quantity, 0)
-              .toFixed(2)}
-          </h5>
-          <button
-            className="btn full-width cart-btn"
-            onClick={checkoutHandler}
-            disabled={cartItems.length === 0}
-          >
-            Proceed to Checkout
-          </button>
-        </Card>
-      </Col>
-    </Row>
+    <div className="cart-container">
+      <Row>
+        <Col md={8} className="cart-info-column">
+          <h2 className="mb-3">Shopping Cart</h2>
+          {cartItems.length === 0 ? (
+            <Message>
+              Your cart is empty <Link to="/">Go Back</Link>
+            </Message>
+          ) : (
+            cartItems.map((item) => {
+              return (
+                <Row key={item.product} className="cart-info-item">
+                  <Col md={1} sm={1} xs={1}>
+                    <img
+                      src={item.images && item.images[0]}
+                      alt={item.name}
+                      className="cart-image"
+                    />
+                  </Col>
+                  <Col md={5} sm={5} xs={5}>
+                    <Link
+                      to={`/products/${item.product}`}
+                      className="cart-product-name"
+                    >
+                      {item.name}
+                    </Link>
+                  </Col>
+                  <Col md={3} sm={3} xs={3}>
+                    ${item.price}
+                  </Col>
+                  <Col md={2} sm={2} xs={2}>
+                    <FormControl
+                      as="select"
+                      value={item.quantity}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </FormControl>
+                  </Col>
+                  <Col md={1} sm={1} xs={1}>
+                    <button
+                      type="button"
+                      className="cart-remove-item-btn"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className="fa fa-trash"></i>
+                    </button>
+                  </Col>
+                </Row>
+              );
+            })
+          )}
+        </Col>
+        <Col md={4}>
+          <Card className="cart-card">
+            <h5 className="cart-title">
+              Subtotal (
+              {cartItems.reduce((acc, curr) => acc + curr.quantity, 0)}{" "}
+              {cartItems.length === 1 ? "item" : "items"}) : $
+              {cartItems
+                .reduce((acc, curr) => acc + curr.price * curr.quantity, 0)
+                .toFixed(2)}
+            </h5>
+            <button
+              className="btn full-width cart-btn"
+              onClick={checkoutHandler}
+              disabled={cartItems.length === 0}
+            >
+              Proceed to Checkout
+            </button>
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 }
 
