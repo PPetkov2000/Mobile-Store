4781e00c1d87f383af8f950f80d7b3451beef052
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-02-06 14:10:47 +0200

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,12 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="py-5 mt-5 bg-dark">
-      <div className="container">
-        <p className="m-0 text-center text-white">
-          Copyright © ng-mobile-store 2020
-        </p>
-      </div>
+    <footer>
+      <p>Copyright © mobile-store 2020</p>
     </footer>
   );
 }
