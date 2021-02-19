19bfbc753c22afb9e1181aabdb498cab4f9eb734
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-02-20 01:18:50 +0200

    update class names

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,24 +5,27 @@ import Rating from "./Rating";
 function Product({ product }) {
   return (
     <div className="product-card">
-      <Link to={`/products/${product._id}`} className="product-card-img-a">
+      <Link
+        to={`/products/${product._id}`}
+        className="product-card__image-wrapper"
+      >
         <img
           src={product.images[0]}
-          className="product-card-img"
+          className="product-card__image"
           alt={product.name}
         />
         <img
           src={product.images[1]}
-          className="product-card-img"
+          className="product-card__image"
           alt={product.name}
         />
       </Link>
-      <h5 className="product-card-title">{product.name}</h5>
-      <h6 className="product-card-subtitle">
+      <h5 className="product-card__title">{product.name}</h5>
+      <h6 className="product-card__subtitle">
         {product.memory}, {product.battery}
       </h6>
       <Rating value={product.rating} />
-      <h5 className="product-card-text">${product.price}</h5>
+      <h5 className="product-card__text">${product.price}</h5>
     </div>
   );
 }
