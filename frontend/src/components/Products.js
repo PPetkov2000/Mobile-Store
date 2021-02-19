9636ba48306188cf750fa874b03507b90be4b1e2
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-02-20 01:18:32 +0200

    update class names

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -5,11 +5,11 @@ import Product from "./Product";
 
 function Products({ title, icon, products, page, pages, keyword }) {
   return (
-    <section className="products-container">
-      <h2 className="products-title">
+    <section className="products__container">
+      <h2 className="products__title">
         {icon} {title}
       </h2>
-      <div className="products-content">
+      <div className="products__content">
         <Row xs={1} sm={2} md={3} lg={4} xl={4}>
           {products.map((product) => {
             return (
