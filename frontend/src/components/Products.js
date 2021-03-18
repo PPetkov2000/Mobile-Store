951f6be2bb57e58e4deb4246280383c2054c1fd4
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-03-19 00:48:12 +0200

    Update Products.js

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -3,25 +3,20 @@ import { Row, Col } from "react-bootstrap";
 import Paginate from "./Paginate";
 import Product from "./Product";
 
-function Products({ title, icon, products, page, pages, keyword }) {
+function Products({ products, page, pages, keyword }) {
   return (
-    <section className="products__container">
-      <h2 className="products__title">
-        {icon} {title}
-      </h2>
-      <div className="products__content">
-        <Row xs={1} sm={2} md={3} lg={4} xl={4}>
-          {products.map((product) => {
-            return (
-              <Col key={product._id}>
-                <Product product={product} />
-              </Col>
-            );
-          })}
-        </Row>
-        {page && <Paginate page={page} pages={pages} keyword={keyword} />}
-      </div>
-    </section>
+    <>
+      <Row xs={1} sm={2} md={3} lg={4} xl={4}>
+        {products.map((product) => {
+          return (
+            <Col key={product._id}>
+              <Product product={product} />
+            </Col>
+          );
+        })}
+      </Row>
+      {page && <Paginate page={page} pages={pages} keyword={keyword} />}
+    </>
   );
 }
 
