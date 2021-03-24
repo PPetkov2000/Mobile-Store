f9fb5655e93dc7b759dfa7ecb622debf934a3d14
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-03-24 14:44:07 +0200

    Update ProductReviewForm.js

--- frontend/src/components/ProductReviewForm.js
+++ frontend/src/components/ProductReviewForm.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
 import { Form } from "react-bootstrap";
 import {
   createProductReview,
@@ -18,9 +17,6 @@ function ProductReviewForm({ productId }) {
   const [comment, setComment] = useState("");
 
   const dispatch = useDispatch();
-
-  const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo } = userLogin;
 
   const productReviewCreate = useSelector((state) => state.productReviewCreate);
   const { loading, error, success } = productReviewCreate;
@@ -50,46 +46,40 @@ function ProductReviewForm({ productId }) {
           <Message variant="success">Review submitted successfully</Message>
         )}
       </div>
-      {userInfo ? (
-        <Form onSubmit={submitHandler}>
-          <Form.Group controlId="rating">
-            <Form.Label>Rating</Form.Label>
-            <Form.Control
-              as="select"
-              value={rating}
-              onChange={(e) => setRating(e.target.value)}
-            >
-              <option value="">Select...</option>
-              <option value="1">1 - Poor</option>
-              <option value="2">2 - Fair</option>
-              <option value="3">3 - Good</option>
-              <option value="4">4 - Very Good</option>
-              <option value="5">5 - Excellent</option>
-            </Form.Control>
-          </Form.Group>
-          <Form.Group controlId="comment">
-            <Form.Label>Comment</Form.Label>
-            <Form.Control
-              as="textarea"
-              row="3"
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
-              placeholder="Enter your comment"
-            ></Form.Control>
-          </Form.Group>
-          <button
-            disabled={loading}
-            type="submit"
-            className="btn btn-main btn-full-width"
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="rating">
+          <Form.Label>Rating</Form.Label>
+          <Form.Control
+            as="select"
+            value={rating}
+            onChange={(e) => setRating(e.target.value)}
           >
-            Submit
-          </button>
-        </Form>
-      ) : (
-        <Message>
-          Please <Link to="/login">sign in</Link> to write a review
-        </Message>
-      )}
+            <option value="">Select...</option>
+            <option value="1">1 - Poor</option>
+            <option value="2">2 - Fair</option>
+            <option value="3">3 - Good</option>
+            <option value="4">4 - Very Good</option>
+            <option value="5">5 - Excellent</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="comment">
+          <Form.Label>Comment</Form.Label>
+          <Form.Control
+            as="textarea"
+            row="3"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="Enter your comment"
+          ></Form.Control>
+        </Form.Group>
+        <button
+          disabled={loading}
+          type="submit"
+          className="btn btn-main btn-full-width"
+        >
+          Submit
+        </button>
+      </Form>
     </>
   );
 }
