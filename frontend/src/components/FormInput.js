cfaf38e3aecb079a1a08f79bc99f0246f8645c94
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-03-31 18:11:35 +0300

    add icon

--- frontend/src/components/FormInput.js
+++ frontend/src/components/FormInput.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 
-function FormInput({ type = "text", name, placeholder, value, handleChange }) {
+function FormInput({
+  type = "text",
+  name,
+  placeholder,
+  value,
+  handleChange,
+  icon,
+}) {
   const capitalize = (str) => {
     return str[0].toUpperCase() + str.slice(1);
   };
 
   return (
     <Form.Group controlId={name}>
-      <Form.Label>{capitalize(name)}</Form.Label>
+      <Form.Label>
+        {icon} {capitalize(name)}
+      </Form.Label>
       <Form.Control
         type={type}
         name={name}
