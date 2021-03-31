e6afac133c91388f5346f33770598f3e16ac576b
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-03-31 17:54:29 +0300

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,17 +8,18 @@ const {
   deleteProduct,
   getTopProducts,
   createProductReview,
+  deleteProductReview,
 } = require("../controllers/productController");
 const { isAuth, isAdmin } = require("../middleware/authMiddleware");
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(isAuth, isAdmin, createProduct);
 router.route("/top").get(getTopProducts);
-router.route("/create").post(isAuth, isAdmin, createProduct);
 router
   .route("/:id")
   .get(getProductById)
   .put(isAuth, isAdmin, updateProduct)
   .delete(isAuth, isAdmin, deleteProduct);
 router.route("/:id/reviews").post(isAuth, createProductReview);
+router.route("/:id/reviews/:reviewId").delete(isAuth, deleteProductReview);
 
 module.exports = router;
