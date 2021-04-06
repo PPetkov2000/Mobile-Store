f1d41c0f14670889626e9e8cc4456c14890b3d09
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-04-07 00:41:47 +0300

    add required attribute

--- frontend/src/components/FormInput.js
+++ frontend/src/components/FormInput.js
@@ -13,10 +13,16 @@ function FormInput({
     return str[0].toUpperCase() + str.slice(1);
   };
 
+  const splitByUppercase = (str) => {
+    return str.match(/[A-Z][a-z]+/g)
+      ? str.match(/[A-Z][a-z]+/g).join(" ")
+      : str;
+  };
+
   return (
     <Form.Group controlId={name}>
       <Form.Label>
-        {icon} {capitalize(name)}
+        {icon} {splitByUppercase(capitalize(name))}
       </Form.Label>
       <Form.Control
         type={type}
@@ -24,6 +30,7 @@ function FormInput({
         placeholder={placeholder}
         value={value}
         onChange={handleChange}
+        required
       ></Form.Control>
     </Form.Group>
   );
