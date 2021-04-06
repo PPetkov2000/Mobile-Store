d313a952aa7b326812f997ce8e0bffedbbfc42bd
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-04-07 00:41:10 +0300

    refactor code

--- frontend/src/screens/admin/ProductEditScreen.js
+++ frontend/src/screens/admin/ProductEditScreen.js
@@ -10,23 +10,26 @@ import {
   updateProduct,
 } from "../../actions/productActions";
 
+const initialFormData = {
+  name: "",
+  images: "",
+  brand: "",
+  price: "",
+  cpu: "",
+  camera: "",
+  size: "",
+  weight: "",
+  display: "",
+  battery: "",
+  memory: "",
+  countInStock: "",
+  description: "",
+};
+
 function ProductEditScreen({ match, history }) {
   const productId = match.params.id;
 
-  const [name, setName] = useState("");
-  const [images, setImages] = useState("");
-  const [brand, setBrand] = useState("");
-  const [price, setPrice] = useState(0);
-  const [cpu, setCpu] = useState("");
-  const [camera, setCamera] = useState("");
-  const [size, setSize] = useState("");
-  const [weight, setWeight] = useState("");
-  const [display, setDisplay] = useState("");
-  const [battery, setBattery] = useState("");
-  const [memory, setMemory] = useState(0);
-  const [countInStock, setCountInStock] = useState(0);
-  const [quantity, setQuantity] = useState(0);
-  const [description, setDescription] = useState("");
+  const [formData, setFormData] = useState(initialFormData);
 
   const dispatch = useDispatch();
 
@@ -48,45 +51,24 @@ function ProductEditScreen({ match, history }) {
       if (!product.name || product._id !== productId) {
         dispatch(listProductDetails(productId));
       } else {
-        setName(product.name);
-        setImages(product.images);
-        setBrand(product.brand);
-        setPrice(product.price);
-        setCpu(product.cpu);
-        setCamera(product.camera);
-        setSize(product.size);
-        setWeight(product.weight);
-        setDisplay(product.display);
-        setBattery(product.battery);
-        setMemory(product.memory);
-        setCountInStock(product.countInStock);
-        setQuantity(product.quantity);
-        setDescription(product.description);
+        const productData = Object.keys(product)
+          .filter((key) => initialFormData.hasOwnProperty(key))
+          .reduce((acc, curr) => {
+            acc[curr] = product[curr];
+            return acc;
+          }, {});
+        setFormData(productData);
       }
     }
   }, [dispatch, history, productId, product, successUpdate]);
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
-    dispatch(
-      updateProduct({
-        _id: productId,
-        name,
-        images,
-        brand,
-        price,
-        cpu,
-        camera,
-        size,
-        weight,
-        display,
-        battery,
-        memory,
-        countInStock,
-        quantity,
-        description,
-      })
-    );
+    dispatch(updateProduct({ _id: productId, ...formData }));
   };
 
   return (
@@ -100,104 +82,16 @@ function ProductEditScreen({ match, history }) {
         <Message variant="danger">{error}</Message>
       ) : (
         <Form onSubmit={submitHandler}>
-          <FormInput
-            type="text"
-            name="Name"
-            placeholder="Enter name"
-            value={name}
-            handleChange={(e) => setName(e.target.value)}
-          />
-          <FormInput
-            type="text"
-            name="Images"
-            placeholder="Enter images"
-            value={images}
-            handleChange={(e) => setImages(e.target.value)}
-          />
-          <FormInput
-            type="text"
-            name="Brand"
-            placeholder="Enter brand"
-            value={brand}
-            handleChange={(e) => setBrand(e.target.value)}
-          />
-          <FormInput
-            type="number"
-            name="Price"
-            placeholder="Enter price"
-            value={price}
-            handleChange={(e) => setPrice(e.target.value)}
-          />
-          <FormInput
-            type="text"
-            name="CPU"
-            placeholder="Enter cpu"
-            value={cpu}
-            handleChange={(e) => setCpu(e.target.value)}
-          />
-          <FormInput
-            type="text"
-            name="Camera"
-            placeholder="Enter camera"
-            value={camera}
-            handleChange={(e) => setCamera(e.target.value)}
-          />
-          <FormInput
-            type="text"
-            name="Size"
-            placeholder="Enter size"
-            value={size}
-            handleChange={(e) => setSize(e.target.value)}
-          />
-          <FormInput
-            type="text"
-            name="Weight"
-            placeholder="Enter weight"
-            value={weight}
-            handleChange={(e) => setWeight(e.target.value)}
-          />
-          <FormInput
-            type="text"
-            name="Display"
-            placeholder="Enter display"
-            value={display}
-            handleChange={(e) => setDisplay(e.target.value)}
-          />
-          <FormInput
-            type="text"
-            name="Battery"
-            placeholder="Enter battery"
-            value={battery}
-            handleChange={(e) => setBattery(e.target.value)}
-          />
-          <FormInput
-            type="text"
-            name="Memory"
-            placeholder="Enter memory"
-            value={memory}
-            handleChange={(e) => setMemory(e.target.value)}
-          />
-          <FormInput
-            type="number"
-            name="Count In Stock"
-            placeholder="Enter count in stock"
-            value={countInStock}
-            handleChange={(e) => setCountInStock(e.target.value)}
-          />
-          <FormInput
-            type="number"
-            name="Quantity"
-            placeholder="Enter quantity"
-            value={quantity}
-            handleChange={(e) => setQuantity(e.target.value)}
-          />
-          <FormInput
-            type="text"
-            name="Description"
-            placeholder="Enter description"
-            value={description}
-            handleChange={(e) => setDescription(e.target.value)}
-          />
+          {Object.keys(initialFormData).map((key) => (
+            <FormInput
+              key={key}
+              type={typeof formData[key] === "number" ? "number" : "text"}
+              name={key}
+              placeholder={`Enter ${key}`}
+              value={formData[key]}
+              handleChange={handleChange}
+            />
+          ))}
           <button type="submit" className="btn btn-main btn-full-width">
             Update
           </button>
