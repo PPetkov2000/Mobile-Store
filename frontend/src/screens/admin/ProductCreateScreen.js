97c943108cee17c91ccce46f70f8d8342e708d7f
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-04-07 00:40:53 +0300

    refactor code

--- frontend/src/screens/admin/ProductCreateScreen.js
+++ frontend/src/screens/admin/ProductCreateScreen.js
@@ -1,31 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router";
 import { createProduct } from "../../actions/productActions";
 import { PRODUCT_CREATE_RESET } from "../../constants/productConstants";
 import FormInput from "../../components/FormInput";
 import Loader from "../../components/Loader";
 import Message from "../../components/Message";
 
-const ProductCreateScreen = () => {
-  const [name, setName] = useState("");
-  const [images, setImages] = useState("");
-  const [brand, setBrand] = useState("");
-  const [price, setPrice] = useState("");
-  const [cpu, setCpu] = useState("");
-  const [camera, setCamera] = useState("");
-  const [size, setSize] = useState("");
-  const [weight, setWeight] = useState("");
-  const [display, setDisplay] = useState("");
-  const [battery, setBattery] = useState("");
-  const [memory, setMemory] = useState("");
-  const [countInStock, setCountInStock] = useState("");
-  const [quantity, setQuantity] = useState("");
-  const [description, setDescription] = useState("");
+const initialFormData = {
+  name: "",
+  images: "",
+  brand: "",
+  price: "",
+  cpu: "",
+  camera: "",
+  size: "",
+  weight: "",
+  display: "",
+  battery: "",
+  memory: "",
+  countInStock: "",
+  description: "",
+};
+
+const ProductCreateScreen = ({ history }) => {
+  const [formData, setFormData] = useState(initialFormData);
 
   const dispatch = useDispatch();
-  const history = useHistory();
 
   const productCreate = useSelector((state) => state.productCreate);
   const { loading, error, success, product } = productCreate;
@@ -37,31 +38,13 @@ const ProductCreateScreen = () => {
     }
   }, [dispatch, history, success, product]);
 
-  const submitHandler = async (e) => {
-    e.preventDefault();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-    try {
-      dispatch(
-        createProduct({
-          name,
-          images,
-          brand,
-          price,
-          cpu,
-          camera,
-          size,
-          weight,
-          display,
-          battery,
-          memory,
-          countInStock,
-          quantity,
-          description,
-        })
-      );
-    } catch (error) {
-      console.log(error);
-    }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(createProduct(formData));
   };
 
   return (
@@ -70,104 +53,16 @@ const ProductCreateScreen = () => {
       {loading && <Loader />}
       {error && <Message variant="danger">{error}</Message>}
       <Form onSubmit={submitHandler}>
-        <FormInput
-          type="text"
-          name="name"
-          placeholder="Enter name"
-          value={name}
-          handleChange={(e) => setName(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          name="images"
-          placeholder="Enter images urls (start with /images/:name)"
-          value={images}
-          handleChange={(e) => setImages(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          name="brand"
-          placeholder="Enter brand"
-          value={brand}
-          handleChange={(e) => setBrand(e.target.value)}
-        />
-        <FormInput
-          type="number"
-          name="price"
-          placeholder="Enter price"
-          value={price}
-          handleChange={(e) => setPrice(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          name="cpu"
-          placeholder="Enter cpu"
-          value={cpu}
-          handleChange={(e) => setCpu(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          name="camera"
-          placeholder="Enter camera"
-          value={camera}
-          handleChange={(e) => setCamera(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          name="size"
-          placeholder="Enter size"
-          value={size}
-          handleChange={(e) => setSize(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          name="weight"
-          placeholder="Enter weight"
-          value={weight}
-          handleChange={(e) => setWeight(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          name="display"
-          placeholder="Enter display"
-          value={display}
-          handleChange={(e) => setDisplay(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          name="battery"
-          placeholder="Enter battery"
-          value={battery}
-          handleChange={(e) => setBattery(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          name="memory"
-          placeholder="Enter memory"
-          value={memory}
-          handleChange={(e) => setMemory(e.target.value)}
-        />
-        <FormInput
-          type="number"
-          name="count in stock"
-          placeholder="Enter count in stock"
-          value={countInStock}
-          handleChange={(e) => setCountInStock(e.target.value)}
-        />
-        <FormInput
-          type="number"
-          name="quantity"
-          placeholder="Enter quantity"
-          value={quantity}
-          handleChange={(e) => setQuantity(e.target.value)}
-        />
-        <FormInput
-          type="text"
-          name="description"
-          placeholder="Enter description"
-          value={description}
-          handleChange={(e) => setDescription(e.target.value)}
-        />
+        {Object.keys(initialFormData).map((key) => (
+          <FormInput
+            key={key}
+            type={typeof formData[key] === "number" ? "number" : "text"}
+            name={key}
+            placeholder={`Enter ${key}`}
+            value={formData[key]}
+            handleChange={handleChange}
+          />
+        ))}
         <button type="submit" className="btn btn-main btn-full-width">
           Create
         </button>
