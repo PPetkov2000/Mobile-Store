86c73bf61a5ce8e36bfb18b41b412a91b2c9d7ee
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-04-24 18:39:05 +0300

    add requests limiter

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const { getUsers, getUserById, updateUser, deleteUser, getUserProfile, updateUserProfile, registerUser, authUser } = require("../controllers/userController");
 const { isAuth, isAdmin } = require("../middleware/authMiddleware");
+const { createAccountLimiter } = require('../utils/rateLimiter');
 
 router.route("/").get(isAuth, isAdmin, getUsers);
 router.route("/profile").get(isAuth, getUserProfile).put(isAuth, updateUserProfile);
 router.route("/:id").get(isAuth, isAdmin, getUserById).put(isAuth, isAdmin, updateUser).delete(isAuth, isAdmin, deleteUser);
-router.route("/register").post(registerUser);
+router.route("/register").post(createAccountLimiter, registerUser);
 router.route("/login").post(authUser);
 
 module.exports = router;
