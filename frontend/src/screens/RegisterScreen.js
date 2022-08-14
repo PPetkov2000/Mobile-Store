1f5edad189ae24e49df0460d23f73f4bdcd3551c
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-08-14 16:29:26 +0300

    Update RegisterScreen.js

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,62 +1,99 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { Form, Row, Col } from "react-bootstrap";
-import { register } from "../actions/userActions";
-import Loader from "../components/Loader";
-import Message from "../components/Message";
-import FormInput from "../components/FormInput";
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Form, Row, Col } from 'react-bootstrap'
+import { register } from '../actions/userActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import FormInput from '../components/FormInput'
+import useForm from '../customHooks/useForm'
 
 function RegisterScreen({ history, location }) {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [message, setMessage] = useState("");
+  const dispatch = useDispatch()
+  const { loading, error, userInfo } = useSelector((state) => state.userLogin)
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+  const { formData, handleChange } = useForm({ username: '', email: '', password: '', confirmPassword: '' })
 
-  const dispatch = useDispatch();
+  const inputs = [
+    {
+      id: 1,
+      type: 'text',
+      name: 'username',
+      label: 'Username',
+      placeholder: 'Enter Username',
+      pattern: '^[A-Za-z0-9]{3,16}$',
+      errorMessage: 'Username should be 3-16 characters long and should not include any special characters!',
+      required: true,
+    },
+    {
+      id: 2,
+      type: 'email',
+      name: 'email',
+      label: 'Email',
+      placeholder: 'Enter Email',
+      errorMessage: 'Please enter a valid email address!',
+      required: true,
+    },
+    {
+      id: 3,
+      type: 'password',
+      name: 'password',
+      label: 'Password',
+      placeholder: 'Enter Password',
+      pattern: '^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,30}$',
+      errorMessage: 'Password should be 6-30 characters long and include at least 1 letter, 1 number, 1 special character!',
+      required: true,
+    },
+    {
+      id: 4,
+      type: 'password',
+      name: 'confirmPassword',
+      label: 'Confirm Password',
+      placeholder: 'Enter Password again',
+      pattern: formData.password,
+      errorMessage: 'Passwords do not match!',
+      required: true,
+    },
+  ]
 
-  const userLogin = useSelector((state) => state.userLogin);
-  const { loading, error, userInfo } = userLogin;
-
-  const redirect = location.search ? location.search.split("=")[1] : "/";
+  const submitHandler = (e) => {
+    e.preventDefault()
+    const emptyFields = Object.keys(formData)
+      .map((key) => formData[key])
+      .every(Boolean)
+    if (emptyFields) return
+    dispatch(register(...formData))
+  }
 
   useEffect(() => {
     if (userInfo) {
-      history.push(redirect);
+      history.push(redirect)
     }
-  }, [history, redirect, userInfo]);
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    if (password !== confirmPassword) {
-      setMessage("Passwords do not match!");
-    } else {
-      dispatch(register(username, email, password, confirmPassword));
-    }
-  };
+  }, [history, redirect, userInfo])
 
   return (
     <div className="auth__container">
       <h2 className="text-center">Sign Up</h2>
-      {message && <Message variant="danger">{message}</Message>}
       {error && <Message variant="danger">{error}</Message>}
       {loading && <Loader />}
       <Form onSubmit={submitHandler} className="auth-form">
-        <FormInput type="text" name="username" placeholder="Enter username" value={username} handleChange={(e) => setUsername(e.target.value)} />
-        <FormInput type="email" name="email" placeholder="Enter email" value={email} handleChange={(e) => setEmail(e.target.value)} />
-        <FormInput type="password" name="password" placeholder="Enter password" value={password} handleChange={(e) => setPassword(e.target.value)} />
-        <FormInput type="password" name="confirmPassword" placeholder="Confirm Password" value={confirmPassword} handleChange={(e) => setConfirmPassword(e.target.value)} />
-        <button type="submit" className="btn btn-main btn-full-width">Register</button>
+        {inputs.map((input) => (
+          <FormInput key={input.id} {...input} value={formData[input.name]} handleChange={handleChange} />
+        ))}
+        <button type="submit" className="btn btn-main btn-full-width">
+          Register
+        </button>
         <Row className="py-3">
-          <Col>Have an Account? <Link to="/login">Login</Link></Col>
+          <Col>
+            Have an Account? <Link to="/login">Login</Link>
+          </Col>
         </Row>
       </Form>
     </div>
-  );
+  )
 }
 
-export default RegisterScreen;
+export default RegisterScreen
 
 /* CLASS IMPLEMENTATION */
 // import { connect } from "react-redux";
