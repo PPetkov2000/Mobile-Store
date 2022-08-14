44dd967376e9a7c604698daa645ea14c517b573b
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-08-14 16:28:43 +0300

    Update OrderScreen.js

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,68 +1,60 @@
-import React, { useEffect, useState } from "react";
-import { Row, Col } from "react-bootstrap";
-import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { PayPalButton } from "react-paypal-button-v2";
-import { getOrderDetails } from "../actions/orderActions";
-import Loader from "../components/Loader";
-import Message from "../components/Message";
-import { ORDER_PAY_RESET } from "../constants/orderConstans";
-import axios from "axios";
-import { payOrder } from "../actions/orderActions";
+import React, { useEffect, useState } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { PayPalButton } from 'react-paypal-button-v2'
+import { getOrderDetails } from '../actions/orderActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { ORDER_PAY_RESET } from '../constants/orderConstans'
+import axios from 'axios'
+import { payOrder } from '../actions/orderActions'
 
 function OrderScreen({ match, history }) {
-  const orderId = match.params.id;
-
-  const [sdkReady, setSdkReady] = useState(false);
-
-  const dispatch = useDispatch();
-
-  const orderDetails = useSelector((state) => state.orderDetails);
-  const { loading, error, order } = orderDetails;
-
-  const orderPay = useSelector((state) => state.orderPay);
-  const { loading: loadingPay, success: successPay } = orderPay;
+  const dispatch = useDispatch()
+  const { loading, error, order } = useSelector((state) => state.orderDetails)
+  const { loading: loadingPay, success: successPay } = useSelector((state) => state.orderPay)
+  const [sdkReady, setSdkReady] = useState(false)
+  const orderId = match.params.id
 
   if (!loading) {
-    const formatPrice = (price) => {
-      return (Math.round(price * 100) / 100).toFixed(2);
-    };
+    const formatPrice = (price) => (Math.round(price * 100) / 100).toFixed(2)
 
-    order.itemsPrice = formatPrice(order.orderItems.reduce((acc, curr) => acc + curr.quantity * curr.price, 0));
-    order.shippingPrice = formatPrice(order.shippingPrice);
-    order.taxPrice = formatPrice(order.taxPrice);
-    order.totalPrice = formatPrice(order.totalPrice);
+    order.itemsPrice = formatPrice(order.orderItems.reduce((acc, curr) => acc + curr.quantity * curr.price, 0))
+    order.shippingPrice = formatPrice(order.shippingPrice)
+    order.taxPrice = formatPrice(order.taxPrice)
+    order.totalPrice = formatPrice(order.totalPrice)
   }
 
   useEffect(() => {
     const addPaypalScript = async () => {
-      const { data } = await axios.get("/api/config/paypal");
-      const script = document.createElement("script");
-      script.type = "text/javascript";
-      script.src = `https://www.paypal.com/sdk/js?client-id=${data}`;
-      script.async = true;
+      const { data } = await axios.get('/api/config/paypal')
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.src = `https://www.paypal.com/sdk/js?client-id=${data}`
+      script.async = true
       script.onload = () => {
-        setSdkReady(true);
-      };
-      document.body.appendChild(script);
-    };
+        setSdkReady(true)
+      }
+      document.body.appendChild(script)
+    }
 
     if (!order || successPay || order._id !== orderId) {
-      dispatch({ type: ORDER_PAY_RESET });
-      dispatch(getOrderDetails(orderId));
+      dispatch({ type: ORDER_PAY_RESET })
+      dispatch(getOrderDetails(orderId))
     } else if (!order.isPaid) {
       if (!window.paypal) {
-        addPaypalScript();
+        addPaypalScript()
       } else {
-        setSdkReady(true);
+        setSdkReady(true)
       }
     }
-  }, [dispatch, history, order, orderId, successPay]);
+  }, [history, order, orderId, successPay])
 
   const successPaymentHandler = (paymentResult) => {
-    console.log(paymentResult);
-    dispatch(payOrder(orderId, paymentResult));
-  };
+    console.log(paymentResult)
+    dispatch(payOrder(orderId, paymentResult))
+  }
 
   return loading ? (
     <Loader />
@@ -77,22 +69,28 @@ function OrderScreen({ match, history }) {
         <Col lg={8} xl={8}>
           <div className="order-info__details">
             <h3 className="order-info__title">Shipping</h3>
-            <p className="mb-0"><strong>Username: </strong>{order.creator.username}</p>
-            <p className="mb-0"><strong>Email: </strong>{order.creator.email}</p>
+            <p className="mb-0">
+              <strong>Username: </strong>
+              {order.creator.username}
+            </p>
+            <p className="mb-0">
+              <strong>Email: </strong>
+              {order.creator.email}
+            </p>
             <p className="mb-0">
               <strong>Address: </strong>
-              {order.shippingAddress.address}, {order.shippingAddress.city},{" "}
-              {order.shippingAddress.postalCode},{" "}
-              {order.shippingAddress.country}
+              {order.shippingAddress.address}, {order.shippingAddress.city}, {order.shippingAddress.postalCode}, {order.shippingAddress.country}
             </p>
           </div>
           <div className="order-info__details">
             <h3 className="order-info__title">Payment Method</h3>
-            <p><strong>Method: </strong>{order.paymentMethod}</p>
+            <p>
+              <strong>Method: </strong>
+              {order.paymentMethod}
+            </p>
             {order.isPaid ? (
               <Message variant="success">
-                <i className="fa fa-check" aria-hidden="true"></i> Paid on{" "}
-                {new Date(Number(order.paidAt)).toLocaleDateString()}
+                <i className="fa fa-check" aria-hidden="true"></i> Paid on {new Date(Number(order.paidAt)).toLocaleDateString()}
               </Message>
             ) : (
               <Message variant="danger">
@@ -111,9 +109,13 @@ function OrderScreen({ match, history }) {
                     <img src={item.imageUrl} alt={item.name} className="order-info__product-image" />
                   </Col>
                   <Col>
-                    <Link to={`/products/${item.product}`} className="order-info__product-name">{item.name}</Link>
+                    <Link to={`/products/${item.product}`} className="order-info__product-name">
+                      {item.name}
+                    </Link>
                   </Col>
-                  <Col xs={5} sm={5} md={5}>{item.quantity} * {item.price} = ${item.quantity * item.price}</Col>
+                  <Col xs={5} sm={5} md={5}>
+                    {item.quantity} * {item.price} = ${item.quantity * item.price}
+                  </Col>
                 </Row>
               ))
             )}
@@ -135,24 +137,24 @@ function OrderScreen({ match, history }) {
               <Col className="text-right">${order.taxPrice}</Col>
             </Row>
             <Row className="order-info__summary-total-price">
-              <Col md={4} sm={6} xs={6}>Total</Col>
-              <Col md={8} sm={6} xs={6} className="text-right">${order.totalPrice}</Col>
+              <Col md={4} sm={6} xs={6}>
+                Total
+              </Col>
+              <Col md={8} sm={6} xs={6} className="text-right">
+                ${order.totalPrice}
+              </Col>
             </Row>
             {!order.isPaid && (
               <Row className="order-info__paypal-button-wrapper">
                 {loadingPay && <Loader />}
-                {!sdkReady ? (
-                  <Loader />
-                ) : (
-                  <PayPalButton amount={order.totalPrice} onSuccess={successPaymentHandler} />
-                )}
+                {!sdkReady ? <Loader /> : <PayPalButton amount={order.totalPrice} onSuccess={successPaymentHandler} />}
               </Row>
             )}
           </div>
         </Col>
       </Row>
     </section>
-  );
+  )
 }
 
-export default OrderScreen;
+export default OrderScreen
