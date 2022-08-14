9a76d4f8fdf7f3b703b991f52be436555dcfe2cc
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-08-14 16:28:12 +0300

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,25 +1,22 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import Loader from "../components/Loader";
-import Message from "../components/Message";
-import Products from "../components/Products";
-import ProductCarousel from "../components/ProductCarousel";
-import { listProducts } from "../actions/productActions";
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import Products from '../components/Products'
+import ProductCarousel from '../components/ProductCarousel'
+import { listProducts } from '../actions/productActions'
 
 function HomeScreen({ match }) {
-  const keyword = match.params.keyword;
-  const pageNumber = match.params.pageNumber || 1;
-
-  const dispatch = useDispatch();
-
-  const productList = useSelector((state) => state.productList);
-  const { loading, error, products, page, pages } = productList;
+  const dispatch = useDispatch()
+  const { loading, error, products, page, pages } = useSelector((state) => state.productList)
+  const keyword = match.params.keyword
+  const pageNumber = match.params.pageNumber || 1
 
   useEffect(() => {
-    dispatch(listProducts(keyword, pageNumber));
-  }, [dispatch, keyword, pageNumber]);
+    dispatch(listProducts(keyword, pageNumber))
+  }, [keyword, pageNumber])
 
   return (
     <>
@@ -35,14 +32,16 @@ function HomeScreen({ match }) {
         ) : products.length === 0 ? (
           <div className="products__empty">
             <h2 className="products__empty-text">Products not found</h2>
-            <Link to="/" className="btn btn-light">Go Back</Link>
+            <Link to="/" className="btn btn-light">
+              Go Back
+            </Link>
           </div>
         ) : (
-          <Products products={products} page={page} pages={pages} keyword={keyword ? keyword : ""} />
+          <Products products={products} page={page} pages={pages} keyword={keyword ? keyword : ''} />
         )}
       </Container>
     </>
-  );
+  )
 }
 
-export default HomeScreen;
+export default HomeScreen
