15875b12a3deeef064c732b90553a205be9caee7
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-08-14 16:27:57 +0300

    Update ShippingScreen.js

--- frontend/src/screens/cart/ShippingScreen.js
+++ frontend/src/screens/cart/ShippingScreen.js
@@ -1,26 +1,65 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Row, Col, Form } from "react-bootstrap";
-import { saveShippingAddress } from "../../actions/cartActions";
-import CheckoutSteps from "../../components/CheckoutSteps";
-import FormInput from "../../components/FormInput";
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Row, Col, Form } from 'react-bootstrap'
+import { saveShippingAddress } from '../../actions/cartActions'
+import CheckoutSteps from '../../components/CheckoutSteps'
+import FormInput from '../../components/FormInput'
+import useForm from '../../customHooks/useForm'
 
 function ShippingScreen({ history }) {
-  const cart = useSelector((state) => state.cart);
-  const { shippingAddress } = cart;
+  const dispatch = useDispatch()
+  const { shippingAddress } = useSelector((state) => state.cart)
+  const { formData, handleChange } = useForm({
+    address: shippingAddress?.address || '',
+    city: shippingAddress?.city || '',
+    postalCode: shippingAddress?.postalCode || '',
+    country: shippingAddress?.country || '',
+  })
 
-  const [address, setAddress] = useState(shippingAddress.address || "");
-  const [city, setCity] = useState(shippingAddress.city || "");
-  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode || "");
-  const [country, setCountry] = useState(shippingAddress.country || "");
-
-  const dispatch = useDispatch();
+  const inputs = [
+    {
+      id: 1,
+      type: 'text',
+      name: 'address',
+      label: 'Address',
+      placeholder: 'Enter Address',
+      icon: 'fa fa-address-book',
+      required: true,
+    },
+    {
+      id: 2,
+      type: 'text',
+      name: 'city',
+      label: 'City',
+      placeholder: 'Enter City',
+      icon: 'fa fa-home',
+      required: true,
+    },
+    {
+      id: 3,
+      type: 'text',
+      name: 'postalCode',
+      label: 'Postal Code',
+      placeholder: 'Enter Postal Code',
+      icon: 'fa fa-envelope-open',
+      required: true,
+    },
+    {
+      id: 4,
+      type: 'text',
+      name: 'country',
+      label: 'Country',
+      placeholder: 'Enter Country',
+      icon: 'fa fa-globe',
+      required: true,
+    },
+  ]
 
   const submitHandler = (e) => {
-    e.preventDefault();
-    dispatch(saveShippingAddress({ address, city, postalCode, country }));
-    history.push("/payment");
-  };
+    e.preventDefault()
+    dispatch(saveShippingAddress(formData))
+    history.push('/payment')
+  }
 
   return (
     <>
@@ -29,44 +68,17 @@ function ShippingScreen({ history }) {
         <Col xs={12} md={8} className="mx-auto">
           <h2>Shipping</h2>
           <Form onSubmit={submitHandler} className="shipping-address__form">
-            <FormInput
-              type="text"
-              name="address"
-              placeholder="Enter address"
-              value={address}
-              handleChange={(e) => setAddress(e.target.value)}
-              icon={<i key={address} className="fa fa-address-book" aria-hidden="true"></i>}
-            />
-            <FormInput
-              type="text"
-              name="city"
-              placeholder="Enter city"
-              value={city}
-              handleChange={(e) => setCity(e.target.value)}
-              icon={<i key={city} className="fa fa-home" aria-hidden="true"></i>}
-            />
-            <FormInput
-              type="text"
-              name="postal code"
-              placeholder="Enter postal code"
-              value={postalCode}
-              handleChange={(e) => setPostalCode(e.target.value)}
-              icon={<i key={postalCode} className="fa fa-envelope-open" aria-hidden="true"></i>}
-            />
-            <FormInput
-              type="text"
-              name="country"
-              placeholder="Enter country"
-              value={country}
-              handleChange={(e) => setCountry(e.target.value)}
-              icon={<i key={country} className="fa fa-globe" aria-hidden="true"></i>}
-            />
-            <button type="submit" className="btn btn-main btn-full-width">Continue</button>
+            {inputs.map((input) => (
+              <FormInput key={input.id} {...input} value={formData[input.name]} handleChange={handleChange} />
+            ))}
+            <button type="submit" className="btn btn-main btn-full-width">
+              Continue
+            </button>
           </Form>
         </Col>
       </Row>
     </>
-  );
+  )
 }
 
-export default ShippingScreen;
+export default ShippingScreen
