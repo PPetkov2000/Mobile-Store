e6a577b9e96e057bbdc3343c800cadba0aa8db01
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-08-14 16:25:44 +0300

    Update UserListScreen.js

--- frontend/src/screens/admin/UserListScreen.js
+++ frontend/src/screens/admin/UserListScreen.js
@@ -1,54 +1,57 @@
-import React, { useEffect } from "react";
-import { Table } from "react-bootstrap";
-import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { deleteUser, listUsers } from "../../actions/userActions";
-import Loader from "../../components/Loader";
-import Message from "../../components/Message";
-import Paginate from "../../components/Paginate";
+import React, { useEffect } from 'react'
+import { Table } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { deleteUser, listUsers } from '../../actions/userActions'
+import Loader from '../../components/Loader'
+import Message from '../../components/Message'
+import Paginate from '../../components/Paginate'
 
 function UserListScreen({ history, match }) {
-  const pageNumber = match.params.pageNumber || 1;
-
-  const dispatch = useDispatch();
-
-  const userList = useSelector((state) => state.userList);
-  const { loading, error, users, page, pages } = userList;
-
-  const userDelete = useSelector((state) => state.userDelete);
-  const { success: successDelete } = userDelete;
+  const dispatch = useDispatch()
+  const { loading, error, users, page, pages } = useSelector((state) => state.userList)
+  const { success: successDelete } = useSelector((state) => state.userDelete)
+  const pageNumber = match.params.pageNumber || 1
 
   useEffect(() => {
-    dispatch(listUsers(pageNumber));
-  }, [dispatch, history, successDelete, pageNumber]);
+    dispatch(listUsers(pageNumber))
+  }, [dispatch, history, successDelete, pageNumber])
 
   const deleteHandler = (userId) => {
-    if (window.confirm("Are you sure?")) {
-      dispatch(deleteUser(userId));
+    if (window.confirm('Are you sure?')) {
+      dispatch(deleteUser(userId))
     }
-  };
+  }
 
   return (
     <div className="admin__container">
       <h2>Users</h2>
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant="danger">{error}</Message>
-      ) : (
-        <div className="table-wrapper">
-          <Table striped bordered hover responsive size="sm">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>NAME</th>
-                <th>EMAIL</th>
-                <th>ADMIN</th>
-                <th></th>
+      <div className="table-wrapper">
+        <Table striped bordered hover responsive size="sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>EMAIL</th>
+              <th>ADMIN</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {loading ? (
+              <tr style={{ height: '300px' }}>
+                <td colSpan="5" style={{ paddingTop: '100px' }}>
+                  <Loader />
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
+            ) : error ? (
+              <tr style={{ height: '300px' }}>
+                <td colSpan="5" style={{ paddingTop: '100px' }}>
+                  <Message variant="danger">{error}</Message>
+                </td>
+              </tr>
+            ) : (
+              users.map((user) => (
                 <tr key={user._id}>
                   <td>{user._id}</td>
                   <td>{user.username}</td>
@@ -56,11 +59,7 @@ function UserListScreen({ history, match }) {
                     <a href={`mailto:${user.email}`}>{user.email}</a>
                   </td>
                   <td className="text-center">
-                    {user.isAdmin ? (
-                      <i className="fa fa-check btn-green--icon"></i>
-                    ) : (
-                      <i className="fa fa-times btn-red--icon"></i>
-                    )}
+                    {user.isAdmin ? <i className="fa fa-check btn-green--icon"></i> : <i className="fa fa-times btn-red--icon"></i>}
                   </td>
                   <td>
                     <div className="admin__actions">
@@ -75,14 +74,14 @@ function UserListScreen({ history, match }) {
                     </div>
                   </td>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-          <Paginate page={page} pages={pages} isAdmin={true} adminPage="userlist" />
-        </div>
-      )}
+              ))
+            )}
+          </tbody>
+        </Table>
+        <Paginate page={page} pages={pages} isAdmin={true} adminPage="userlist" />
+      </div>
     </div>
-  );
+  )
 }
 
-export default UserListScreen;
+export default UserListScreen
