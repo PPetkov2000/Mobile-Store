66c34b97287ac84ed50ed6e23467e9ecdffe470d
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-08-14 16:26:45 +0300

    Update OrderListScreen.js

--- frontend/src/screens/admin/OrderListScreen.js
+++ frontend/src/screens/admin/OrderListScreen.js
@@ -1,57 +1,58 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Table } from "react-bootstrap";
-import Loader from "../../components/Loader";
-import Message from "../../components/Message";
-import { listOrders } from "../../actions/orderActions";
-import { Link } from "react-router-dom";
-import Paginate from "../../components/Paginate";
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Table } from 'react-bootstrap'
+import Loader from '../../components/Loader'
+import Message from '../../components/Message'
+import { listOrders } from '../../actions/orderActions'
+import { Link } from 'react-router-dom'
+import Paginate from '../../components/Paginate'
 
 function OrderListScreen({ match }) {
-  const pageNumber = match.params.pageNumber || 1;
-
-  const dispatch = useDispatch();
-
-  const orderList = useSelector((state) => state.orderList);
-  const { loading, error, orders, page, pages } = orderList;
+  const dispatch = useDispatch()
+  const { loading, error, orders, page, pages } = useSelector((state) => state.orderList)
+  const pageNumber = match.params.pageNumber || 1
 
   useEffect(() => {
-    dispatch(listOrders(pageNumber));
-  }, [dispatch, pageNumber]);
+    dispatch(listOrders(pageNumber))
+  }, [dispatch, pageNumber])
 
   return (
-    <div className="admin-container">
+    <div className="admin__container">
       <h2>Orders</h2>
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant="danger">{error}</Message>
-      ) : (
-        <div className="table-wrapper">
-          <Table striped bordered hover responsive className="table-sm">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>USER</th>
-                <th>DATE</th>
-                <th>TOTAL</th>
-                <th>PAID</th>
-                <th></th>
+      <div className="table-wrapper">
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>USER</th>
+              <th>DATE</th>
+              <th>TOTAL</th>
+              <th>PAID</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {loading ? (
+              <tr style={{ height: '300px' }}>
+                <td colSpan="6" style={{ paddingTop: '100px' }}>
+                  <Loader />
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {orders.map((order) => (
+            ) : error ? (
+              <tr style={{ height: '300px' }}>
+                <td colSpan="6" style={{ paddingTop: '100px' }}>
+                  <Message variant="danger">{error}</Message>
+                </td>
+              </tr>
+            ) : (
+              orders.map((order) => (
                 <tr key={order._id}>
                   <td>{order._id}</td>
                   <td>{order.creator && order.creator.username}</td>
                   <td>{order.createdAt.substring(0, 10)}</td>
                   <td>${order.totalPrice.toFixed(2)}</td>
                   <td className="text-center">
-                    {order.isPaid ? (
-                      new Date(Number(order.paidAt)).toLocaleDateString()
-                    ) : (
-                      <i className="fa fa-times btn-red--icon"></i>
-                    )}
+                    {order.isPaid ? new Date(Number(order.paidAt)).toLocaleDateString() : <i className="fa fa-times btn-red--icon"></i>}
                   </td>
                   <td>
                     <Link to={`/order/${order._id}`} className="d-flex justify-content-center">
@@ -59,14 +60,14 @@ function OrderListScreen({ match }) {
                     </Link>
                   </td>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-          <Paginate page={page} pages={pages} isAdmin={true} adminPage="orderlist" />
-        </div>
-      )}
+              ))
+            )}
+          </tbody>
+        </Table>
+        <Paginate page={page} pages={pages} isAdmin={true} adminPage="orderlist" />
+      </div>
     </div>
-  );
+  )
 }
 
-export default OrderListScreen;
+export default OrderListScreen
