7468c18012da264b59eeb0339734a38041b992a4
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-08-14 16:25:57 +0300

    Update ProductListScreen.js

--- frontend/src/screens/admin/ProductListScreen.js
+++ frontend/src/screens/admin/ProductListScreen.js
@@ -1,32 +1,27 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Table } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import Loader from "../../components/Loader";
-import Message from "../../components/Message";
-import { deleteProduct, listProducts } from "../../actions/productActions";
-import Paginate from "../../components/Paginate";
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Table } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Loader from '../../components/Loader'
+import Message from '../../components/Message'
+import { deleteProduct, listProducts } from '../../actions/productActions'
+import Paginate from '../../components/Paginate'
 
 function ProductListScreen({ match, history }) {
-  const pageNumber = match.params.pageNumber || 1;
-
-  const dispatch = useDispatch();
-
-  const productList = useSelector((state) => state.productList);
-  const { loading, error, products, page, pages } = productList;
-
-  const productDelete = useSelector((state) => state.productDelete);
-  const { loading: loadingDelete, error: errorDelete, success: successDelete } = productDelete;
+  const dispatch = useDispatch()
+  const { loading, error, products, page, pages } = useSelector((state) => state.productList)
+  const { loading: loadingDelete, error: errorDelete, success: successDelete } = useSelector((state) => state.productDelete)
+  const pageNumber = match.params.pageNumber || 1
 
   useEffect(() => {
-    dispatch(listProducts("", pageNumber));
-  }, [dispatch, history, successDelete, pageNumber]);
+    dispatch(listProducts('', pageNumber))
+  }, [history, successDelete, pageNumber])
 
   const deleteProductHandler = (productId) => {
-    if (window.confirm("Are you sure?")) {
-      dispatch(deleteProduct(productId));
+    if (window.confirm('Are you sure?')) {
+      dispatch(deleteProduct(productId))
     }
-  };
+  }
 
   return (
     <div className="admin__container">
@@ -38,24 +33,32 @@ function ProductListScreen({ match, history }) {
       </div>
       {loadingDelete && <Loader />}
       {errorDelete && <Message variant="danger">{errorDelete}</Message>}
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant="danger">{error}</Message>
-      ) : (
-        <div className="table-wrapper">
-          <Table striped bordered hover responsive size="sm">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>NAME</th>
-                <th>PRICE</th>
-                <th>BRAND</th>
-                <th></th>
+      <div className="table-wrapper">
+        <Table striped bordered hover responsive size="sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>PRICE</th>
+              <th>BRAND</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {loading ? (
+              <tr style={{ height: '300px' }}>
+                <td colSpan="5" style={{ paddingTop: '100px' }}>
+                  <Loader />
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {products.map((product) => (
+            ) : error ? (
+              <tr style={{ height: '300px' }}>
+                <td colSpan="5" style={{ paddingTop: '100px' }}>
+                  <Message variant="danger">{error}</Message>
+                </td>
+              </tr>
+            ) : (
+              products.map((product) => (
                 <tr key={product._id}>
                   <td>{product._id}</td>
                   <td>{product.name}</td>
@@ -74,14 +77,14 @@ function ProductListScreen({ match, history }) {
                     </div>
                   </td>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-          <Paginate page={page} pages={pages} isAdmin={true} adminPage="productlist" />
-        </div>
-      )}
+              ))
+            )}
+          </tbody>
+        </Table>
+        <Paginate page={page} pages={pages} isAdmin={true} adminPage="productlist" />
+      </div>
     </div>
-  );
+  )
 }
 
-export default ProductListScreen;
+export default ProductListScreen
