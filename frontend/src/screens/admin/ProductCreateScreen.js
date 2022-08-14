c9d89a97c9a6f755a62ce047a110a856ec91f414
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-08-14 16:26:26 +0300

    Update ProductCreateScreen.js

--- frontend/src/screens/admin/ProductCreateScreen.js
+++ frontend/src/screens/admin/ProductCreateScreen.js
@@ -1,51 +1,45 @@
-import React, { useState, useEffect } from "react";
-import { Form } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { createProduct } from "../../actions/productActions";
-import { PRODUCT_CREATE_RESET } from "../../constants/productConstants";
-import FormInput from "../../components/FormInput";
-import Loader from "../../components/Loader";
-import Message from "../../components/Message";
+import React, { useEffect } from 'react'
+import { Form } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { createProduct } from '../../actions/productActions'
+import { PRODUCT_CREATE_RESET } from '../../constants/productConstants'
+import FormInput from '../../components/FormInput'
+import Loader from '../../components/Loader'
+import Message from '../../components/Message'
+import useForm from '../../customHooks/useForm'
 
 const initialFormData = {
-  name: "",
-  images: "",
-  brand: "",
-  price: "",
-  cpu: "",
-  camera: "",
-  size: "",
-  weight: "",
-  display: "",
-  battery: "",
-  memory: "",
-  countInStock: "",
-  description: "",
-};
+  name: '',
+  images: '',
+  brand: '',
+  price: '',
+  cpu: '',
+  camera: '',
+  size: '',
+  weight: '',
+  display: '',
+  battery: '',
+  memory: '',
+  countInStock: '',
+  description: '',
+}
 
 const ProductCreateScreen = ({ history }) => {
-  const [formData, setFormData] = useState(initialFormData);
-
-  const dispatch = useDispatch();
-
-  const productCreate = useSelector((state) => state.productCreate);
-  const { loading, error, success, product } = productCreate;
+  const dispatch = useDispatch()
+  const { loading, error, success, product } = useSelector((state) => state.productCreate)
+  const { formData, handleChange } = useForm(initialFormData)
 
   useEffect(() => {
     if (success) {
-      dispatch({ type: PRODUCT_CREATE_RESET });
-      history.push("/admin/productlist");
+      dispatch({ type: PRODUCT_CREATE_RESET })
+      history.push('/admin/productlist')
     }
-  }, [dispatch, history, success, product]);
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  }, [success, product])
 
   const submitHandler = (e) => {
-    e.preventDefault();
-    dispatch(createProduct(formData));
-  };
+    e.preventDefault()
+    dispatch(createProduct(formData))
+  }
 
   return (
     <div className="product-create__container">
@@ -56,17 +50,19 @@ const ProductCreateScreen = ({ history }) => {
         {Object.keys(initialFormData).map((key) => (
           <FormInput
             key={key}
-            type={typeof formData[key] === "number" ? "number" : "text"}
+            type={typeof formData[key] === 'number' ? 'number' : 'text'}
             name={key}
             placeholder={`Enter ${key}`}
             value={formData[key]}
             handleChange={handleChange}
           />
         ))}
-        <button type="submit" className="btn btn-main btn-full-width">Create</button>
+        <button type="submit" className="btn btn-main btn-full-width">
+          Create
+        </button>
       </Form>
     </div>
-  );
-};
+  )
+}
 
-export default ProductCreateScreen;
+export default ProductCreateScreen
