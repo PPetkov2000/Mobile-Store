fc4deac7e501f1869d014cb46dbfadf84ece7293
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-08-14 16:27:16 +0300

    Update CartScreen.js

--- frontend/src/screens/cart/CartScreen.js
+++ frontend/src/screens/cart/CartScreen.js
@@ -1,31 +1,29 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { Col, Row, Card, FormControl } from "react-bootstrap";
-import Message from "../../components/Message";
-import { addToCart, removeFromCart } from "../../actions/cartActions";
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { Col, Row, Card, FormControl } from 'react-bootstrap'
+import Message from '../../components/Message'
+import { addToCart, removeFromCart } from '../../actions/cartActions'
 
 function CartScreen({ match, location, history }) {
-  const productId = match.params.id;
-  const dispatch = useDispatch();
-  const cart = useSelector((state) => state.cart);
-  const { cartItems } = cart;
-
-  const quantity = location.search ? Number(location.search.split("=")[1]) : 1;
+  const dispatch = useDispatch()
+  const { cartItems } = useSelector((state) => state.cart)
+  const productId = match.params.id
+  const quantity = location.search ? Number(location.search.split('=')[1]) : 1
 
   useEffect(() => {
     if (productId) {
-      dispatch(addToCart(productId, quantity));
+      dispatch(addToCart(productId, quantity))
     }
-  }, [dispatch, productId, quantity]);
+  }, [dispatch, productId, quantity])
 
   const removeFromCartHandler = (id) => {
-    dispatch(removeFromCart(id));
-  };
+    dispatch(removeFromCart(id))
+  }
 
   const checkoutHandler = () => {
-    history.push("/login?redirect=shipping");
-  };
+    history.push('/login?redirect=shipping')
+  }
 
   return (
     <section className="cart__container">
@@ -33,7 +31,9 @@ function CartScreen({ match, location, history }) {
         <Col md={8} className="cart__info">
           <h2 className="mb-3">Shopping Cart</h2>
           {cartItems.length === 0 ? (
-            <Message>Your cart is empty <Link to="/">Go Back</Link></Message>
+            <Message>
+              Your cart is empty <Link to="/">Go Back</Link>
+            </Message>
           ) : (
             cartItems.map((item) => {
               return (
@@ -42,19 +42,19 @@ function CartScreen({ match, location, history }) {
                     <img src={item.images && item.images[0]} alt={item.name} className="cart__info-image" />
                   </Col>
                   <Col md={5} sm={5} xs={5}>
-                    <Link to={`/products/${item.product}`} className="cart__info-product-name">{item.name}</Link>
+                    <Link to={`/products/${item.product}`} className="cart__info-product-name">
+                      {item.name}
+                    </Link>
                   </Col>
                   <Col md={3} sm={3} xs={3}>
                     ${item.price}
                   </Col>
                   <Col md={2} sm={2} xs={2}>
-                    <FormControl
-                      as="select"
-                      value={item.quantity}
-                      onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}
-                    >
+                    <FormControl as="select" value={item.quantity} onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
                       {[...Array(item.countInStock).keys()].map((x) => (
-                        <option key={x + 1} value={x + 1}>{x + 1}</option>
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
                       ))}
                     </FormControl>
                   </Col>
@@ -64,24 +64,24 @@ function CartScreen({ match, location, history }) {
                     </button>
                   </Col>
                 </Row>
-              );
+              )
             })
           )}
         </Col>
         <Col md={4}>
           <Card className="cart__purchase-info">
             <h5 className="cart__purchase-info-title">
-              Subtotal (
-              {cartItems.reduce((acc, curr) => acc + curr.quantity, 0)}{" "}
-              {cartItems.length === 1 ? "item" : "items"}) : $
+              Subtotal ({cartItems.reduce((acc, curr) => acc + curr.quantity, 0)} {cartItems.length === 1 ? 'item' : 'items'}) : $
               {cartItems.reduce((acc, curr) => acc + curr.price * curr.quantity, 0).toFixed(2)}
             </h5>
-            <button className="btn btn-cart" onClick={checkoutHandler} disabled={cartItems.length === 0}>Proceed to Checkout</button>
+            <button className="btn btn-cart" onClick={checkoutHandler} disabled={cartItems.length === 0}>
+              Proceed to Checkout
+            </button>
           </Card>
         </Col>
       </Row>
     </section>
-  );
+  )
 }
 
-export default CartScreen;
+export default CartScreen
