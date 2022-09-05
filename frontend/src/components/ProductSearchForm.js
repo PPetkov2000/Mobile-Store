965e97cda5f7cf0c3a8ebbd58c7c674f7958b3de
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-09-06 00:00:05 +0300

    Update ProductSearchForm.js

--- frontend/src/components/ProductSearchForm.js
+++ frontend/src/components/ProductSearchForm.js
@@ -1,23 +1,25 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router";
-import { Form, FormControl } from "react-bootstrap";
+import React, { useState } from 'react'
+import { useHistory } from 'react-router'
+import { Form, FormControl } from 'react-bootstrap'
 
 function ProductSearchForm() {
-  const [keyword, setKeyword] = useState("");
-  const history = useHistory();
+  const [keyword, setKeyword] = useState('')
+  const history = useHistory()
 
   const searchHandler = (e) => {
-    e.preventDefault();
-    history.push(`/search/${keyword}`);
-    setKeyword("");
-  };
+    e.preventDefault()
+    history.push(`/search/${keyword}`)
+    setKeyword('')
+  }
 
   return (
     <Form inline className="search-form mx-auto" onSubmit={searchHandler}>
       <FormControl type="text" placeholder="Search..." className="mr-sm-2 searchInput" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
-      <button type="submit" className="btn btn-green--bordered hidden-below-1000">Search</button>
+      <button type="submit" className="btn btn-green--bordered hidden-below-1000">
+        Search
+      </button>
     </Form>
-  );
+  )
 }
 
-export default ProductSearchForm;
+export default ProductSearchForm
