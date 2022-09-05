e41d112a82212e421ee58424426461cc0c6cc1cd
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-09-06 00:02:16 +0300

    Update ProductReview.js

--- frontend/src/components/ProductReview.js
+++ frontend/src/components/ProductReview.js
@@ -1,27 +1,23 @@
-import React from "react";
-import { Badge } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { deleteProductReview } from "../actions/productActions";
-import Loader from "../components/Loader";
-import Message from "../components/Message";
-import Rating from "../components/Rating";
-import ProductReviewForm from "./ProductReviewForm";
+import React from 'react'
+import { Badge } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { deleteProductReview } from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import Rating from '../components/Rating'
+import ProductReviewForm from './ProductReviewForm'
 
 function ProductReview({ productId, productReviews }) {
-  const dispatch = useDispatch();
-
-  const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo } = userLogin;
-
-  const productReviewDelete = useSelector((state) => state.productReviewDelete);
-  const { loading: loadingDelete, error: errorDelete, success: successDelete } = productReviewDelete;
+  const dispatch = useDispatch()
+  const { userInfo } = useSelector((state) => state.userLogin)
+  const { loading: loadingDelete, error: errorDelete, success: successDelete } = useSelector((state) => state.productReviewDelete)
 
   const deleteReviewHandler = () => {
     if (productReviews) {
-      const productReview = productReviews.find((r) => r.creator === userInfo._id);
-      dispatch(deleteProductReview(productId, productReview._id));
+      const productReview = productReviews.find((r) => r.creator === userInfo._id)
+      dispatch(deleteProductReview(productId, productReview._id))
     }
-  };
+  }
 
   return (
     <div className="product-review">
@@ -30,29 +26,27 @@ function ProductReview({ productId, productReviews }) {
       {errorDelete && <Message variant="danger">{errorDelete}</Message>}
       {successDelete && <Message variant="success">Review removed</Message>}
       {productReviews.length === 0 && <Message>No Reviews</Message>}
-      {productReviews.map((review) => {
-        return (
-          <div key={review._id}>
-            <div className="product-review__header">
-              <div className="d-flex">
-                <p className="mb-0 mr-4">{review.name}</p>
-                <Rating value={review.rating} />
-              </div>
-              {userInfo && (review.creator === userInfo._id || userInfo.isAdmin) && (
-                <button className="btn-red--icon" onClick={deleteReviewHandler}>
-                  <i className="fa fa-trash"></i>
-                </button>
-              )}
+      {productReviews.map((review) => (
+        <div key={review._id}>
+          <div className="product-review__header">
+            <div className="d-flex">
+              <p className="mb-0 mr-4">{review.name}</p>
+              <Rating value={review.rating} />
             </div>
-            <div className="product-review__content">
-              <p>
-                {review.comment}
-                <Badge variant="dark">{review.createdAt.substring(0, 10)}</Badge>
-              </p>
-            </div>
+            {userInfo && (review.creator === userInfo._id || userInfo.isAdmin) && (
+              <button className="btn-red--icon" onClick={deleteReviewHandler} title="Remove product button">
+                <i className="fa fa-trash"></i>
+              </button>
+            )}
           </div>
-        );
-      })}
+          <div className="product-review__content">
+            <p>
+              {review.comment}
+              <Badge variant="dark">{review.createdAt.substring(0, 10)}</Badge>
+            </p>
+          </div>
+        </div>
+      ))}
       {productReviews && userInfo && Boolean(!productReviews.find((r) => r.creator.toString() === userInfo._id.toString())) && (
         <>
           <hr className="bg-secondary" />
@@ -60,7 +54,7 @@ function ProductReview({ productId, productReviews }) {
         </>
       )}
     </div>
-  );
+  )
 }
 
-export default ProductReview;
+export default ProductReview
