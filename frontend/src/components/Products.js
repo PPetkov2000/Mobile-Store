0d8a466cd1c86b0f1da501a7f1e17e906c08e78e
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-09-05 23:58:19 +0300

    Update Products.js

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,23 +1,21 @@
-import React from "react";
-import { Row, Col } from "react-bootstrap";
-import Paginate from "./Paginate";
-import Product from "./Product";
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import Paginate from './Paginate'
+import Product from './Product'
 
 function Products({ products, page, pages, keyword }) {
   return (
     <>
       <Row xs={1} sm={2} md={3} lg={4} xl={4}>
-        {products.map((product) => {
-          return (
-            <Col key={product._id}>
-              <Product product={product} />
-            </Col>
-          );
-        })}
+        {products.map((product) => (
+          <Col key={product._id}>
+            <Product product={product} />
+          </Col>
+        ))}
       </Row>
       {page && <Paginate page={page} pages={pages} keyword={keyword} />}
     </>
-  );
+  )
 }
 
-export default Products;
+export default Products
