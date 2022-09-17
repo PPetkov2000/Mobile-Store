128c866bc8ae71254301048617dae49a78b79274
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-09-18 01:20:59 +0300

    Update userActions.js

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
-import api from "../utils/api-instance";
-import { ORDER_LIST_MY_RESET } from "../constants/orderConstans";
+import api from '../utils/api-instance'
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstans'
 import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
@@ -24,95 +24,105 @@ import {
   USER_DELETE_FAIL,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
-  USER_UPDATE_PROFILE_FAIL
-} from "../constants/userConstants";
-import actionsErrorHandler from "../utils/actionsErrorHandler";
+  USER_UPDATE_PROFILE_FAIL,
+} from '../constants/userConstants'
+import actionsErrorHandler from '../utils/actionsErrorHandler'
 
 export const register = (username, email, password, confirmPassword) => async (dispatch) => {
   try {
-    dispatch({ type: USER_REGISTER_REQUEST });
-    const { data } = await api.post("/api/v1/users/register", { username, email, password, confirmPassword });
-    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
-    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
-    localStorage.setItem("userInfo", JSON.stringify(data));
+    dispatch({ type: USER_REGISTER_REQUEST })
+    const { data } = await api.post('/api/v1/users/register', { username, email, password, confirmPassword })
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data })
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data })
+    localStorage.setItem('userInfo', JSON.stringify(data))
   } catch (error) {
-    dispatch({ type: USER_REGISTER_FAIL, payload: actionsErrorHandler(error) });
+    dispatch({ type: USER_REGISTER_FAIL, payload: actionsErrorHandler(error) })
   }
-};
+}
 
 export const login = (email, password) => async (dispatch) => {
   try {
-    dispatch({ type: USER_LOGIN_REQUEST });
-    const { data } = await api.post("/api/v1/users/login", { email, password });
-    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
-    localStorage.setItem("userInfo", JSON.stringify(data));
+    dispatch({ type: USER_LOGIN_REQUEST })
+    const { data } = await api.post('/api/v1/users/login', { email, password })
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data })
+    localStorage.setItem('userInfo', JSON.stringify(data))
   } catch (error) {
-    dispatch({ type: USER_LOGIN_FAIL, payload: actionsErrorHandler(error) });
+    dispatch({ type: USER_LOGIN_FAIL, payload: actionsErrorHandler(error) })
   }
-};
+}
 
 export const logout = () => (dispatch) => {
-  localStorage.removeItem("userInfo");
-  localStorage.removeItem("cartItems");
-  localStorage.removeItem("shippingAddress");
-  localStorage.removeItem("paymentMethod");
-  dispatch({ type: USER_LOGOUT });
-  dispatch({ type: USER_DETAILS_RESET });
-  dispatch({ type: USER_LIST_RESET });
-  dispatch({ type: ORDER_LIST_MY_RESET });
-  document.location.href = "/login";
-};
+  localStorage.removeItem('userInfo')
+  localStorage.removeItem('cartItems')
+  localStorage.removeItem('shippingAddress')
+  localStorage.removeItem('paymentMethod')
+  dispatch({ type: USER_LOGOUT })
+  dispatch({ type: USER_DETAILS_RESET })
+  dispatch({ type: USER_LIST_RESET })
+  dispatch({ type: ORDER_LIST_MY_RESET })
+  document.location.href = '/login'
+}
 
-export const listUsers = (pageNumber = "") => async (dispatch) => {
-  try {
-    dispatch({ type: USER_LIST_REQUEST });
-    const { data } = await api.get(`/api/v1/users?pageNumber=${pageNumber}`);
-    dispatch({ type: USER_LIST_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({ type: USER_LIST_FAIL, payload: actionsErrorHandler(error) });
+const listUsersCache = new Map()
+export const listUsers =
+  (pageNumber = '') =>
+  async (dispatch) => {
+    try {
+      dispatch({ type: USER_LIST_REQUEST })
+      if (!listUsersCache.has(pageNumber)) {
+        const { data } = await api.get(`/api/v1/users?pageNumber=${pageNumber}`)
+        listUsersCache.set(pageNumber, data)
+      }
+      dispatch({ type: USER_LIST_SUCCESS, payload: listUsersCache.get(pageNumber) })
+    } catch (error) {
+      dispatch({ type: USER_LIST_FAIL, payload: actionsErrorHandler(error) })
+    }
   }
-};
 
+const getUserDetailsCache = new Map()
 export const getUserDetails = (userId) => async (dispatch) => {
   try {
-    dispatch({ type: USER_DETAILS_REQUEST });
-    const { data } = await api.get(`/api/v1/users/${userId}`);
-    dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
+    dispatch({ type: USER_DETAILS_REQUEST })
+    if (!getUserDetailsCache.has(userId)) {
+      const { data } = await api.get(`/api/v1/users/${userId}`)
+      getUserDetailsCache.set(userId, data)
+    }
+    dispatch({ type: USER_DETAILS_SUCCESS, payload: getUserDetailsCache.get(userId) })
   } catch (error) {
-    dispatch({ type: USER_DETAILS_FAIL, payload: actionsErrorHandler(error) });
+    dispatch({ type: USER_DETAILS_FAIL, payload: actionsErrorHandler(error) })
   }
-};
+}
 
 export const updateUser = (user) => async (dispatch) => {
   try {
-    dispatch({ type: USER_UPDATE_REQUEST });
-    const { data } = await api.put(`/api/v1/users/${user._id}`, user);
-    dispatch({ type: USER_UPDATE_SUCCESS });
-    dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
-    dispatch({ type: USER_DETAILS_RESET });
+    dispatch({ type: USER_UPDATE_REQUEST })
+    const { data } = await api.put(`/api/v1/users/${user._id}`, user)
+    dispatch({ type: USER_UPDATE_SUCCESS })
+    dispatch({ type: USER_DETAILS_SUCCESS, payload: data })
+    dispatch({ type: USER_DETAILS_RESET })
   } catch (error) {
-    dispatch({ type: USER_UPDATE_FAIL, payload: actionsErrorHandler(error) });
+    dispatch({ type: USER_UPDATE_FAIL, payload: actionsErrorHandler(error) })
   }
-};
+}
 
 export const deleteUser = (userId) => async (dispatch) => {
   try {
-    dispatch({ type: USER_DELETE_REQUEST });
-    await api.delete(`/api/v1/users/${userId}`);
-    dispatch({ type: USER_DELETE_SUCCESS });
+    dispatch({ type: USER_DELETE_REQUEST })
+    await api.delete(`/api/v1/users/${userId}`)
+    dispatch({ type: USER_DELETE_SUCCESS })
   } catch (error) {
-    dispatch({ type: USER_DELETE_FAIL, payload: actionsErrorHandler(error) });
+    dispatch({ type: USER_DELETE_FAIL, payload: actionsErrorHandler(error) })
   }
-};
+}
 
 export const updateUserProfile = (user) => async (dispatch) => {
   try {
-    dispatch({ type: USER_UPDATE_PROFILE_REQUEST });
-    const { data } = await api.put(`/api/v1/users/profile`, user);
-    dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data });
-    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
-    localStorage.setItem("userInfo", JSON.stringify(data));
+    dispatch({ type: USER_UPDATE_PROFILE_REQUEST })
+    const { data } = await api.put(`/api/v1/users/profile`, user)
+    dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data })
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data })
+    localStorage.setItem('userInfo', JSON.stringify(data))
   } catch (error) {
-    dispatch({ type: USER_UPDATE_PROFILE_FAIL, payload: actionsErrorHandler(error) });
+    dispatch({ type: USER_UPDATE_PROFILE_FAIL, payload: actionsErrorHandler(error) })
   }
-};
+}
