c3e996e1c1d2a5331c486535addd0c68cad62a2e
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-09-18 01:20:00 +0300

    Update orderActions.js

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,5 +1,5 @@
-import api from "../utils/api-instance";
-import { CART_CLEAR_ITEMS } from "../constants/cartConstants";
+import api from '../utils/api-instance'
+import { CART_CLEAR_ITEMS } from '../constants/cartConstants'
 import {
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
@@ -16,57 +16,73 @@ import {
   ORDER_PAY_REQUEST,
   ORDER_PAY_SUCCESS,
   ORDER_PAY_FAIL,
-} from "../constants/orderConstans";
-import actionsErrorHandler from "../utils/actionsErrorHandler";
+} from '../constants/orderConstans'
+import actionsErrorHandler from '../utils/actionsErrorHandler'
 
 export const createOrder = (order) => async (dispatch) => {
   try {
-    dispatch({ type: ORDER_CREATE_REQUEST });
-    const { data } = await api.post("/api/v1/orders", order);
-    dispatch({ type: ORDER_CREATE_SUCCESS, payload: data });
-    dispatch({ type: CART_CLEAR_ITEMS, payload: data });
-    localStorage.removeItem("cartItems");
+    dispatch({ type: ORDER_CREATE_REQUEST })
+    const { data } = await api.post('/api/v1/orders', order)
+    dispatch({ type: ORDER_CREATE_SUCCESS, payload: data })
+    dispatch({ type: CART_CLEAR_ITEMS, payload: data })
+    localStorage.removeItem('cartItems')
   } catch (error) {
-    dispatch({ type: ORDER_CREATE_FAIL, payload: actionsErrorHandler(error) });
+    dispatch({ type: ORDER_CREATE_FAIL, payload: actionsErrorHandler(error) })
   }
-};
+}
 
-export const listOrders = (pageNumber = "") => async (dispatch) => {
-  try {
-    dispatch({ type: ORDER_LIST_REQUEST });
-    const { data } = await api.get(`/api/v1/orders?pageNumber=${pageNumber}`);
-    dispatch({ type: ORDER_LIST_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({ type: ORDER_LIST_FAIL, payload: actionsErrorHandler(error) });
+const listOrdersCache = new Map()
+export const listOrders =
+  (pageNumber = '') =>
+  async (dispatch) => {
+    try {
+      dispatch({ type: ORDER_LIST_REQUEST })
+      if (!listOrdersCache.has(pageNumber)) {
+        const { data } = await api.get(`/api/v1/orders?pageNumber=${pageNumber}`)
+        listOrdersCache.set(pageNumber, data)
+      }
+      dispatch({ type: ORDER_LIST_SUCCESS, payload: listOrdersCache.get(pageNumber) })
+    } catch (error) {
+      dispatch({ type: ORDER_LIST_FAIL, payload: actionsErrorHandler(error) })
+    }
   }
-};
 
-export const listMyOrders = (pageNumber = "") => async (dispatch) => {
-  try {
-    dispatch({ type: ORDER_LIST_MY_REQUEST });
-    const { data } = await api.get(`/api/v1/orders/myorders?pageNumber=${pageNumber}`);
-    dispatch({ type: ORDER_LIST_MY_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({ type: ORDER_LIST_MY_FAIL, payload: actionsErrorHandler(error) });
+const listMyOrdersCache = new Map()
+export const listMyOrders =
+  (pageNumber = '') =>
+  async (dispatch) => {
+    try {
+      dispatch({ type: ORDER_LIST_MY_REQUEST })
+      if (!listMyOrdersCache.has(pageNumber)) {
+        const { data } = await api.get(`/api/v1/orders/myorders?pageNumber=${pageNumber}`)
+        listMyOrdersCache.set(pageNumber, data)
+      }
+      dispatch({ type: ORDER_LIST_MY_SUCCESS, payload: listMyOrdersCache.get(pageNumber) })
+    } catch (error) {
+      dispatch({ type: ORDER_LIST_MY_FAIL, payload: actionsErrorHandler(error) })
+    }
   }
-};
 
+const getOrderDetailsCache = new Map()
 export const getOrderDetails = (orderId) => async (dispatch) => {
   try {
-    dispatch({ type: ORDER_DETAILS_REQUEST });
-    const { data } = await api.get(`/api/v1/orders/${orderId}`);
-    dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data });
+    dispatch({ type: ORDER_DETAILS_REQUEST })
+    if (!getOrderDetailsCache.has(orderId)) {
+      const { data } = await api.get(`/api/v1/orders/${orderId}`)
+      getOrderDetailsCache.set(orderId, data)
+    }
+    dispatch({ type: ORDER_DETAILS_SUCCESS, payload: getOrderDetailsCache.get(orderId) })
   } catch (error) {
-    dispatch({ type: ORDER_DETAILS_FAIL, payload: actionsErrorHandler(error) });
+    dispatch({ type: ORDER_DETAILS_FAIL, payload: actionsErrorHandler(error) })
   }
-};
+}
 
 export const payOrder = (orderId, paymentResult) => async (dispatch) => {
   try {
-    dispatch({ type: ORDER_PAY_REQUEST });
-    const { data } = await api.put(`/api/v1/orders/${orderId}/pay`, paymentResult);
-    dispatch({ type: ORDER_PAY_SUCCESS, payload: data });
+    dispatch({ type: ORDER_PAY_REQUEST })
+    const { data } = await api.put(`/api/v1/orders/${orderId}/pay`, paymentResult)
+    dispatch({ type: ORDER_PAY_SUCCESS, payload: data })
   } catch (error) {
-    dispatch({ type: ORDER_PAY_FAIL, payload: actionsErrorHandler(error) });
+    dispatch({ type: ORDER_PAY_FAIL, payload: actionsErrorHandler(error) })
   }
-};
+}
