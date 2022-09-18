8df7621daa3a3070f1631459060b879b83b9a1ef
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-09-18 17:44:17 +0300

    Update ShippingScreen.js

--- frontend/src/screens/cart/ShippingScreen.js
+++ frontend/src/screens/cart/ShippingScreen.js
@@ -5,6 +5,7 @@ import { saveShippingAddress } from '../../actions/cartActions'
 import CheckoutSteps from '../../components/CheckoutSteps'
 import FormInput from '../../components/FormInput'
 import useForm from '../../customHooks/useForm'
+import { SHIPPING_FIELDS } from '../../configs/form-fields'
 
 function ShippingScreen({ history }) {
   const dispatch = useDispatch()
@@ -15,45 +16,6 @@ function ShippingScreen({ history }) {
     postalCode: shippingAddress?.postalCode || '',
     country: shippingAddress?.country || '',
   })
-
-  const inputs = [
-    {
-      id: 1,
-      type: 'text',
-      name: 'address',
-      label: 'Address',
-      placeholder: 'Enter Address',
-      icon: 'fa fa-address-book',
-      required: true,
-    },
-    {
-      id: 2,
-      type: 'text',
-      name: 'city',
-      label: 'City',
-      placeholder: 'Enter City',
-      icon: 'fa fa-home',
-      required: true,
-    },
-    {
-      id: 3,
-      type: 'text',
-      name: 'postalCode',
-      label: 'Postal Code',
-      placeholder: 'Enter Postal Code',
-      icon: 'fa fa-envelope-open',
-      required: true,
-    },
-    {
-      id: 4,
-      type: 'text',
-      name: 'country',
-      label: 'Country',
-      placeholder: 'Enter Country',
-      icon: 'fa fa-globe',
-      required: true,
-    },
-  ]
 
   const submitHandler = (e) => {
     e.preventDefault()
@@ -68,7 +30,7 @@ function ShippingScreen({ history }) {
         <Col xs={12} md={8} className="mx-auto">
           <h2>Shipping</h2>
           <Form onSubmit={submitHandler} className="shipping-address__form">
-            {inputs.map((input) => (
+            {SHIPPING_FIELDS(formData).map((input) => (
               <FormInput key={input.id} {...input} value={formData[input.name]} handleChange={handleChange} />
             ))}
             <button type="submit" className="btn btn-main btn-full-width">
