c9c64677bc4b4ba527f3f21cf874b86ad8007cbc
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-09-23 00:45:33 +0300

    Lazy load routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,32 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import "./App.scss";
-import PrivateRoute from "./router/PrivateRoute";
-import AdminRoute from "./router/AdminRoute";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Forbidden from "./components/Forbidden";
-import PageNotFound from "./components/PageNotFound";
-import ErrorBoundary from "./components/ErrorBoundary";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-import LoginScreen from "./screens/LoginScreen";
-import CartScreen from "./screens/cart/CartScreen";
-import ShippingScreen from "./screens/cart/ShippingScreen";
-import PaymentScreen from "./screens/cart/PaymentScreen";
-import PlaceOrderScreen from "./screens/cart/PlaceOrderScreen";
-import OrderScreen from "./screens/OrderScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import UserListScreen from "./screens/admin/UserListScreen";
-import UserEditScreen from "./screens/admin/UserEditScreen";
-import ProductListScreen from "./screens/admin/ProductListScreen";
-import ProductEditScreen from "./screens/admin/ProductEditScreen";
-import OrderListScreen from "./screens/admin/OrderListScreen";
-import ProductCreateScreen from "./screens/admin/ProductCreateScreen";
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Container, Spinner } from 'react-bootstrap'
+import './App.scss'
+import PrivateRoute from './router/PrivateRoute'
+import AdminRoute from './router/AdminRoute'
+import Header from './components/Header'
+import Loader from './components/Loader'
+import ErrorBoundary from './components/ErrorBoundary'
+
+const HomeScreen = lazy(() => import('./screens/HomeScreen'))
+const ProductScreen = lazy(() => import('./screens/ProductScreen'))
+const RegisterScreen = lazy(() => import('./screens/RegisterScreen'))
+const LoginScreen = lazy(() => import('./screens/LoginScreen'))
+const CartScreen = lazy(() => import('./screens/cart/CartScreen'))
+const ShippingScreen = lazy(() => import('./screens/cart/ShippingScreen'))
+const PaymentScreen = lazy(() => import('./screens/cart/PaymentScreen'))
+const PlaceOrderScreen = lazy(() => import('./screens/cart/PlaceOrderScreen'))
+const OrderScreen = lazy(() => import('./screens/OrderScreen'))
+const ProfileScreen = lazy(() => import('./screens/ProfileScreen'))
+const UserListScreen = lazy(() => import('./screens/admin/UserListScreen'))
+const UserEditScreen = lazy(() => import('./screens/admin/UserEditScreen'))
+const ProductListScreen = lazy(() => import('./screens/admin/ProductListScreen'))
+const ProductEditScreen = lazy(() => import('./screens/admin/ProductEditScreen'))
+const OrderListScreen = lazy(() => import('./screens/admin/OrderListScreen'))
+const ProductCreateScreen = lazy(() => import('./screens/admin/ProductCreateScreen'))
+const Footer = lazy(() => import('./components/Footer'))
+const Forbidden = lazy(() => import('./components/Forbidden'))
+const PageNotFound = lazy(() => import('./components/PageNotFound'))
 
 function App() {
   return (
@@ -32,36 +35,40 @@ function App() {
       <main className="py-5">
         <ErrorBoundary>
           <Switch>
-            <Route path={["/", "/search/:keyword", "/page/:pageNumber", "/search/:keyword/page/:pageNumber"]} exact component={HomeScreen} />
-            <Container>
-              <Route path="/products/:id" component={ProductScreen} />
-              <Route path="/cart/:id?" component={CartScreen} />
-              <PrivateRoute path="/order/:id" component={OrderScreen} />
-              <Route path="/shipping" component={ShippingScreen} />
-              <Route path="/payment" component={PaymentScreen} />
-              <Route path="/placeorder" component={PlaceOrderScreen} />
-              <PrivateRoute path="/profile" exact component={ProfileScreen} />
-              <PrivateRoute path="/profile/page/:pageNumber" exact component={ProfileScreen} />
-              <Route path="/register" component={RegisterScreen} />
-              <Route path="/login" component={LoginScreen} />
-              <AdminRoute path="/admin/userlist" exact component={UserListScreen} />
-              <AdminRoute path="/admin/userlist/:pageNumber" exact component={UserListScreen} />
-              <AdminRoute path="/admin/user/:id/edit" component={UserEditScreen} />
-              <AdminRoute path="/admin/productlist" exact component={ProductListScreen} />
-              <AdminRoute path="/admin/productlist/:pageNumber" exact component={ProductListScreen} />
-              <AdminRoute path="/admin/product/:id/edit" component={ProductEditScreen} />
-              <AdminRoute path="/admin/product/create" exact component={ProductCreateScreen} />
-              <AdminRoute path="/admin/orderlist" exact component={OrderListScreen} />
-              <AdminRoute path="/admin/orderlist/:pageNumber" exact component={OrderListScreen} />
-              <Route path="/forbidden" component={Forbidden} />
-            </Container>
-            <Route path="*" component={PageNotFound} />
+            <Suspense fallback={<Loader />}>
+              <Route path={['/', '/search/:keyword', '/page/:pageNumber', '/search/:keyword/page/:pageNumber']} exact component={HomeScreen} />
+              <Container>
+                <Route path="/products/:id" component={ProductScreen} />
+                <Route path="/cart/:id?" component={CartScreen} />
+                <PrivateRoute path="/order/:id" component={OrderScreen} />
+                <Route path="/shipping" component={ShippingScreen} />
+                <Route path="/payment" component={PaymentScreen} />
+                <Route path="/placeorder" component={PlaceOrderScreen} />
+                <PrivateRoute path="/profile" exact component={ProfileScreen} />
+                <PrivateRoute path="/profile/page/:pageNumber" exact component={ProfileScreen} />
+                <Route path="/register" component={RegisterScreen} />
+                <Route path="/login" component={LoginScreen} />
+                <AdminRoute path="/admin/userlist" exact component={UserListScreen} />
+                <AdminRoute path="/admin/userlist/:pageNumber" exact component={UserListScreen} />
+                <AdminRoute path="/admin/user/:id/edit" component={UserEditScreen} />
+                <AdminRoute path="/admin/productlist" exact component={ProductListScreen} />
+                <AdminRoute path="/admin/productlist/:pageNumber" exact component={ProductListScreen} />
+                <AdminRoute path="/admin/product/:id/edit" component={ProductEditScreen} />
+                <AdminRoute path="/admin/product/create" exact component={ProductCreateScreen} />
+                <AdminRoute path="/admin/orderlist" exact component={OrderListScreen} />
+                <AdminRoute path="/admin/orderlist/:pageNumber" exact component={OrderListScreen} />
+                <Route path="/forbidden" component={Forbidden} />
+              </Container>
+              <Route path="*" component={PageNotFound} />
+            </Suspense>
           </Switch>
         </ErrorBoundary>
       </main>
-      <Footer />
+      <Suspense fallback={<Spinner />}>
+        <Footer />
+      </Suspense>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
