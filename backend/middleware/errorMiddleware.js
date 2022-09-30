4a2fe1f1bac9c7a5012175ccd8da8dad02234d04
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-09-30 23:43:28 +0300

    Update errorMiddleware.js

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,16 +1,20 @@
 const notFound = (req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-};
+  const error = new Error(`Not Found - ${req.originalUrl}`)
+  res.status(404)
+  next(error)
+}
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
+  if (err.message.includes('Cast to ObjectId failed')) {
+    res.status(400).json({ message: err.reason.message })
+    return
+  }
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  res.status(statusCode)
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
-};
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  })
+}
 
-module.exports = { notFound, errorHandler };
+module.exports = { notFound, errorHandler }
