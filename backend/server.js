95a775c35d9db2ab80f0b25373a5e98f955a7046
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-10-04 01:26:34 +0300

    add cache

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,43 @@
-const express = require("express");
-const logger = require("morgan");
-const helmet = require("helmet");
-const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-const productRoutes = require("./routes/productRoutes");
-const userRoutes = require("./routes/userRoutes");
-const orderRoutes = require("./routes/orderRoutes");
-const connectDB = require("./config/database");
-const { rateLimiter } = require("./utils/rateLimiter")
+const express = require('express')
+const logger = require('morgan')
+const connectDB = require('./config/database')
+const setCache = require('./middleware/setCache')
+const { rateLimiter } = require('./utils/rateLimiter')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+const helmet = require('helmet')
+const productRoutes = require('./routes/productRoutes')
+const userRoutes = require('./routes/userRoutes')
+const orderRoutes = require('./routes/orderRoutes')
 
-require("dotenv").config();
-connectDB();
+require('dotenv').config()
+connectDB()
 
-const app = express();
+const app = express()
 
-rateLimiter(app);
+rateLimiter(app)
 
-app.use(helmet());
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(helmet())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(setCache())
 
-app.use("/api/v1/products", productRoutes);
-app.use("/api/v1/orders", orderRoutes);
-app.use("/api/v1/users", userRoutes);
-app.use("/api/v1/config/paypal", (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
-});
+app.use('/api/v1/products', productRoutes)
+app.use('/api/v1/orders', orderRoutes)
+app.use('/api/v1/users', userRoutes)
+app.use('/api/v1/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
+})
 
-app.use(notFound);
-app.use(errorHandler);
+app.use(notFound)
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 6000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const PORT = process.env.PORT || 6000
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
 
 // const data = require("./data");
 // const Product = require("./models/ProductModel");
-// 
+//
 // app.use("/api/products/seed", async (req, res) => {
 //   try {
 //     const products = await Product.insertMany(data.products);
