a889458f79ffcb763521a6bb4ff1900bbcc00dcc
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-12-12 22:36:02 +0200

    Update Products.js

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -3,7 +3,7 @@ import { Row, Col } from 'react-bootstrap'
 import Paginate from './Paginate'
 import Product from './Product'
 
-function Products({ products, page, pages, keyword }) {
+function Products({ products = [], page, pages, keyword = '' }) {
   return (
     <>
       <Row xs={1} sm={2} md={3} lg={4} xl={4}>
