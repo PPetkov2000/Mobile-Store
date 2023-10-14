a929d3f1df9eb9e6059c70d5257ae470830b9645
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2023-10-15 00:00:57 +0300

    Make the input type search

--- frontend/src/components/ProductSearchForm.js
+++ frontend/src/components/ProductSearchForm.js
@@ -14,7 +14,7 @@ function ProductSearchForm() {
 
   return (
     <Form inline className="search-form mx-auto" onSubmit={searchHandler}>
-      <FormControl type="text" placeholder="Search..." className="mr-sm-2 searchInput" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+      <FormControl type="search" placeholder="Search..." className="mr-sm-2 searchInput" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
       <button type="submit" className="btn btn-green--bordered hidden-below-1000">
         Search
       </button>
